b46e2cf4f33822b9b6f7bd2920e21374028ac0b3
Gautier <gautier@escape.tech>
2022-06-25 15:44:44 +0200

    feat: registration, validation and auth

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -4,9 +4,12 @@ import {
   YogaInitialContext,
 } from "@graphql-yoga/node";
 import SchemaBuilder from "@pothos/core";
+import ComplexityPlugin from "@pothos/plugin-complexity";
 import PrismaPlugin from "@pothos/plugin-prisma";
+import ScopeAuthPlugin from "@pothos/plugin-scope-auth";
+import ValidationPlugin from "@pothos/plugin-validation";
 import { CredentialType, PrismaClient } from "@prisma/client";
-import { verify } from "argon2";
+import argon2 from "argon2";
 import { writeFile } from "fs/promises";
 import { printSchema } from "graphql";
 import { DateTimeResolver } from "graphql-scalars";
@@ -17,13 +20,18 @@ const prisma = new PrismaClient({ log: ["query"], rejectOnNotFound: true });
 
 const builder = new SchemaBuilder<{
   PrismaTypes: PrismaTypes;
-  Context: YogaInitialContext;
+  Context: YogaInitialContext & Awaited<ReturnType<typeof context>>;
   DefaultInputFieldRequiredness: true;
   Scalars: { DateTime: { Input: Date; Output: Date } };
+  AuthScopes: { loggedIn: boolean };
 }>({
-  plugins: [PrismaPlugin],
+  plugins: [ComplexityPlugin, PrismaPlugin, ScopeAuthPlugin, ValidationPlugin],
   prisma: { client: prisma },
   defaultInputFieldRequiredness: true,
+  complexity: { limit: { complexity: 100, depth: 3, breadth: 30 } },
+  authScopes: ({ user }) => ({
+    loggedIn: Boolean(user),
+  }),
 });
 
 const UserType = builder.prismaObject("User", {
@@ -67,6 +75,7 @@ builder.queryType({
   fields: (t) => ({
     users: t.prismaField({
       type: ["User"],
+      authScopes: { loggedIn: true },
       resolve: (query) => prisma.user.findMany(query),
     }),
   }),
@@ -86,7 +95,7 @@ builder.mutationType({
         });
 
         for (const { value, userId } of credentials) {
-          if (await verify(value, password)) {
+          if (await argon2.verify(value, password)) {
             const { value: token } = await prisma.credential.create({
               data: { type: CredentialType.Token, value: nanoid(), userId },
             });
@@ -94,14 +103,59 @@ builder.mutationType({
           }
         }
 
-        throw new GraphQLYogaError("Incorrect credentials ");
+        throw new GraphQLYogaError("Incorrect credentials");
+      },
+    }),
+    register: t.prismaField({
+      type: "User",
+      args: {
+        name: t.arg.string({
+          validate: {
+            minLength: 3,
+            maxLength: 20,
+            regex: [/[a-z][a-z_.-]*/, { message: "Letters, -, _ and . only" }],
+          },
+        }),
+        firstname: t.arg.string({ validate: { minLength: 1, maxLength: 255 } }),
+        lastname: t.arg.string({ validate: { minLength: 1, maxLength: 255 } }),
+        password: t.arg.string({ validate: { minLength: 10, maxLength: 255 } }),
+      },
+      async resolve(query, _, { name, firstname, lastname, password }) {
+        return prisma.user.create({
+          ...query,
+          data: {
+            name,
+            firstname,
+            lastname,
+            credentials: {
+              create: {
+                type: CredentialType.Password,
+                value: await argon2.hash(password, { type: argon2.argon2id }),
+              },
+            },
+          },
+        });
       },
     }),
   }),
 });
 
+const getUser = async ({ headers }: Request) => {
+  const auth = headers.get("Authorization");
+  if (!auth) return null;
+  const token = auth.slice("Bearer ".length);
+  console.log(token);
+  return prisma.credential
+    .findFirst({ where: { type: CredentialType.Token, value: token } })
+    .user();
+};
+
+const context = async ({ request }: YogaInitialContext) => ({
+  user: await getUser(request),
+});
+
 const schema = builder.toSchema({});
-const server = createServer({ schema });
+const server = createServer({ schema, context, maskedErrors: false });
 
 process.env.DEBUG = "true";
 await server.start();
