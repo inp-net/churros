4cba9aa73e55d1019d0f20faaa61ae376298ca3a
Gautier <gautier@escape.tech>
2022-07-04 19:02:04 +0200

    feat: disallow batched queries

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,10 +1,16 @@
 import { createServer } from "@graphql-yoga/node";
+import { useNoBatchedQueries } from "envelop-no-batched-queries";
 import { context } from "./context.js";
 import { schema, writeSchema } from "./schema.js";
 
 process.env.DEBUG = "true";
 
-const server = createServer({ schema, context, maskedErrors: false });
+const server = createServer({
+  schema,
+  context,
+  maskedErrors: false,
+  plugins: [useNoBatchedQueries()],
+});
 
 await server.start();
 
