7e9d12aa3dcca90f5d8073ef4e708d06249b5e8c
Gautier <gautier@escape.tech>
2022-07-14 12:57:13 +0200

    chore: updated eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -47,6 +47,7 @@ module.exports = {
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-return': 'off',
+        'unicorn/consistent-destructuring': 'off',
       },
     },
   ],
