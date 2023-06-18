31fb418297e4340642dd1c89d01cf9657551b824
whidix <theodor.vallier@gmail.com>
2023-06-18 14:58:34 +0200

    feat: update storybook

--- packages/app/.storybook/main.cjs
+++ packages/app/.storybook/main.cjs
@@ -9,13 +9,17 @@ module.exports = {
     '@storybook/addon-interactions',
     '@storybook/addon-svelte-csf',
   ],
-  framework: '@storybook/svelte',
-  core: { builder: '@storybook/builder-vite' },
-  svelteOptions: async () => (await import('../svelte.config.js')).default,
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {},
+  },
   viteFinal: async (storybookConfig) => {
     const config = mergeConfig((await import('../vite.config.js')).commonConfig, storybookConfig);
     // Waiting for https://github.com/storybookjs/builder-vite/issues/419 to be fixed
     config.plugins = config.plugins.filter((plugin) => plugin.name !== 'svelte-docgen');
     return config;
+  },
+  docs: {
+    autodocs: true,
   },
 };
