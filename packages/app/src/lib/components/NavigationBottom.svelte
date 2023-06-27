0a2b286ec25cede345328c378f52ae666a770a80
Ewen Le Bihan <hey@ewen.works>
2023-06-28 00:11:33 +0200

    feat: implement navigation/bottom

---
+++ packages/app/src/lib/components/NavigationBottom.svelte
@@ -0,0 +1,211 @@
+<script lang="ts">
+  import IconHomeOutline from '~icons/mdi/home-outline';
+  import IconHome from '~icons/mdi/home';
+  import IconSearchOutline from '~icons/mdi/card-search-outline';
+  import IconSearch from '~icons/mdi/card-search';
+  import IconAddCircleOutline from '~icons/mdi/plus-circle-outline';
+  import IconAddCircle from '~icons/mdi/plus-circle';
+  import IconCalendarOutline from '~icons/mdi/calendar-blank-outline';
+  import IconCalendar from '~icons/mdi/calendar';
+  import IconDotsCircleOutline from '~icons/mdi/dots-horizontal-circle-outline';
+  import IconDotsCircle from '~icons/mdi/dots-horizontal-circle';
+
+  import IconBarWeek from '~icons/mdi/beer-outline';
+  import IconGroup from '~icons/mdi/account-group-outline';
+  import IconAnnouncement from '~icons/mdi/bullhorn-outline';
+  import IconDocument from '~icons/mdi/file-outline';
+  import IconArticle from '~icons/mdi/newspaper';
+  import IconEvent from '~icons/mdi/calendar-plus';
+
+  export let current: 'home' | 'search' | 'events' | 'more';
+  let flyoutOpen = false;
+</script>
+
+<nav class:flyout-open={flyoutOpen}>
+  <a href="/" class:current={!flyoutOpen && current === 'home'} class:disabled={flyoutOpen}>
+    {#if current === 'home'}
+      <IconHome />
+    {:else}
+      <IconHomeOutline />
+    {/if}
+  </a>
+
+  <a href="/search" class:current={!flyoutOpen && current === 'search'} class:disabled={flyoutOpen}>
+    {#if current === 'search'}
+      <IconSearch />
+    {:else}
+      <IconSearchOutline />
+    {/if}
+  </a>
+
+  <button
+    class:current={flyoutOpen}
+    on:click={() => {
+      flyoutOpen = !flyoutOpen;
+    }}
+  >
+    {#if flyoutOpen}
+      <IconAddCircle />
+    {:else}
+      <IconAddCircleOutline />
+    {/if}
+  </button>
+
+  <a href="/week" class:current={!flyoutOpen && current === 'events'} class:disabled={flyoutOpen}>
+    {#if current === 'events'}
+      <IconCalendar />
+    {:else}
+      <IconCalendarOutline />
+    {/if}
+  </a>
+
+  <a href="/more" class:current={!flyoutOpen && current === 'more'} class:disabled={flyoutOpen}>
+    {#if current === 'more'}
+      <IconDotsCircle />
+    {:else}
+      <IconDotsCircleOutline />
+    {/if}
+  </a>
+</nav>
+
+<div
+  class="flyout-backdrop"
+  on:click={() => {
+    flyoutOpen = false;
+  }}
+  class:open={flyoutOpen}
+>
+  <section class="flyout" class:open={flyoutOpen}>
+    <a href="/bar-weeks">
+      <IconBarWeek />
+      <span>Semaine de bar</span>
+    </a>
+
+    <a href="/groups">
+      <IconGroup />
+      <span>Groupes</span>
+    </a>
+
+    <a href="/announcements">
+      <IconAnnouncement />
+      <span>Annonces</span>
+    </a>
+
+    <a href="/frappe/upload">
+      <IconDocument />
+      <span>Frappe</span>
+    </a>
+
+    <a href="/articles/create">
+      <IconArticle />
+      <span>Article</span>
+    </a>
+
+    <a href="/events/create">
+      <IconEvent />
+      <span>Événement</span>
+    </a>
+  </section>
+</div>
+
+<style>
+  nav {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 3;
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 4rem;
+    background: var(--bg);
+    border-top: var(--border-block) solid rgb(0 0 0 / 5%);
+  }
+
+  nav.flyout-open {
+    border-top-color: var(--bg);
+  }
+
+  button {
+    padding: 0;
+    margin: 0;
+    color: var(--text);
+    cursor: pointer;
+    background: transparent;
+    border: none;
+  }
+
+  a,
+  button {
+    font-size: 1.25rem;
+    transition: color 0.25s ease;
+  }
+
+  .disabled {
+    color: var(--muted-text);
+  }
+
+  .current {
+    color: var(--primary-bg);
+  }
+
+  .flyout-backdrop.open {
+    position: fixed;
+    inset: 0;
+    z-index: 1;
+    background: rgb(0 0 0 / 50%);
+    animation: fade-in-backdrop 0.2s ease-in-out;
+  }
+
+  @keyframes fade-in-backdrop {
+    from {
+      background: rgb(0 0 0 / 0%);
+    }
+
+    to {
+      background: rgb(0 0 0 / 50%);
+    }
+  }
+
+  .flyout {
+    position: fixed;
+    right: 0;
+    bottom: 4rem;
+    left: 0;
+    z-index: 2;
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-evenly;
+    max-height: 50vh;
+    padding: 1rem;
+    background: var(--bg);
+    border-top: var(--border-block) solid rgb(0 0 0 / 5%);
+    border-top-left-radius: calc(4 * var(--radius-block));
+    border-top-right-radius: calc(4 * var(--radius-block));
+    transition: bottom 0.2s ease-in-out;
+  }
+
+  .flyout:not(.open) {
+    bottom: -100%;
+  }
+
+  .flyout a {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: calc(min(max(5rem, 33%), 10rem));
+    aspect-ratio: 1;
+    font-size: 1.5rem;
+    font-weight: bold;
+    text-align: center;
+  }
+
+  .flyout a span {
+    margin-top: 0.2rem;
+    font-size: 0.8rem;
+  }
+</style>
--- packages/app/src/routes/+layout.svelte
+++ packages/app/src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-  import { navigating, page } from '$app/stores';
+  import { page } from '$app/stores';
   import TopBar from '$lib/components/NavigationTop.svelte';
   import { theme } from '$lib/theme.js';
   import { onMount } from 'svelte';
-  import { tweened } from 'svelte/motion';
   import '../design/app.scss';
   import type { LayoutData } from './$types';
+  import NavigationBottom from '$lib/components/NavigationBottom.svelte';
 
   let { mobile } = $page.data as LayoutData;
   const onResize = () => {
@@ -14,18 +14,18 @@
 
   onMount(onResize);
 
-  let menuOpen = false;
-  let menuWidth = 0;
-  const menuOpenness = tweened(0, { duration: 80 });
-  let start: Touch | undefined;
-  let startedAt = 0;
-  let diff: { x: number; y: number } | undefined;
+  function currentTab(url: URL): 'events' | 'search' | 'more' | 'home' {
+    const starts = (segment: string) => url.pathname.startsWith(segment);
 
-  $: if ($navigating || !mobile) menuOpen = false;
-  $: $menuOpenness = menuOpen ? 1 : 0;
+    if (starts('/search')) return 'search';
+    if (starts('/week') || starts('/bookings') || starts('/events')) return 'events';
+    if (starts('/more')) return 'more';
+    return 'home';
+  }
 
   onMount(() => {
     let currentTheme = $theme;
+    $theme = 'light';
     theme.subscribe(($theme) => {
       if (currentTheme) document.documentElement.classList.remove(currentTheme);
       document.documentElement.classList.add($theme);
@@ -34,70 +34,13 @@
   });
 </script>
 
-<svelte:window
-  on:resize={onResize}
-  on:touchstart={({ touches }) => {
-    if (!mobile || touches.length !== 1) return;
-    start = touches[0];
-    startedAt = window.performance.now();
-    diff = { x: 0, y: 0 };
-  }}
-  on:touchmove={({ changedTouches }) => {
-    if (!mobile || !start || !diff) return;
-    // @ts-expect-error for some reason it thinks that a TouchList is not iterable
-    const touch = [...changedTouches].find(({ identifier }) => identifier === start?.identifier);
-    if (!touch) return;
-
-    diff = { x: touch.clientX - start.clientX, y: touch.clientY - start.clientY };
-    if (Math.abs(diff.x) > 2 * Math.abs(diff.y)) {
-      void menuOpenness.set((menuOpen ? 1 : 0) + diff.x / menuWidth, { duration: 0 });
-    } else {
-      $menuOpenness = menuOpen ? 1 : 0;
-      diff = undefined;
-    }
-  }}
-  on:touchend={({ touches }) => {
-    if (!mobile || !diff) return;
-    // @ts-expect-error for some reason it thinks that a TouchList is not iterable
-    if ([...touches].some(({ identifier }) => identifier === start?.identifier)) return;
-
-    menuOpen =
-      Math.abs(diff.x) > Math.max(2 * Math.abs(diff.y), menuWidth / 20) &&
-      window.performance.now() - startedAt < 300
-        ? diff.x > 0
-        : $menuOpenness >= 0.5;
-    $menuOpenness = menuOpen ? 1 : 0;
-
-    diff = undefined;
-  }}
-/>
+<svelte:window on:resize={onResize} />
 
 <svelte:head>
   <title>Centraverse</title>
 </svelte:head>
 
 <TopBar {mobile} />
-
-{#if mobile}
-  <div
-    class="mobile-background"
-    hidden={$menuOpenness === 0}
-    style:opacity={$menuOpenness}
-    on:touchend={() => {
-      menuOpen = false;
-      $menuOpenness = 0;
-    }}
-  />
-  <div
-    class="mobile-menu"
-    style:transform="translateX(-{(1 - $menuOpenness) * 100}%)"
-    hidden={$menuOpenness === 0}
-    bind:clientWidth={menuWidth}
-  >
-    <div class="flex-shrink-0 h-10" />
-    <div class="flex-shrink flex-1 px-2 overflow-auto overscroll-contain" />
-  </div>
-{/if}
 
 <div class="layout">
   {#if !mobile}
@@ -107,6 +50,8 @@
     <slot />
   </main>
 </div>
+
+<NavigationBottom current={currentTab($page.url)} />
 
 <style lang="scss">
   main {
