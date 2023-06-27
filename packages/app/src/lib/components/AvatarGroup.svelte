1fcc0d8530b51473acc92f396e579e4deefce067
Ewen Le Bihan <hey@ewen.works>
2023-06-27 12:23:40 +0200

    feat: rename, add hover styles, improve alt text display

--- packages/app/src/lib/components/AvatarGroup.svelte
+++ packages/app/src/lib/components/AvatarGroup.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
   import { PUBLIC_STORAGE_URL } from '$env/static/public';
   export let name: string;
+  export let uid: string;
   export let pictureFile: string;
   const src = `${PUBLIC_STORAGE_URL}${pictureFile}`;
 </script>
 
-<div class="group">
+<a href="/club/{uid}" class="group">
   <div class="img">
     <img {src} alt={name} />
   </div>
   <h3 class="name">{name}</h3>
-</div>
+</a>
 
 <style>
   .group {
@@ -20,13 +21,28 @@
     align-items: center;
     width: fit-content;
     padding: 0.5rem;
+    text-decoration: none;
   }
 
   .group .img {
     width: 6rem;
     height: 6rem;
     overflow: hidden;
+    text-align: center;
+
+    line-height: 6rem; /* to vertically center alt text */
+    font-weight: bold;
+    color: var(--muted-text);
+    background-color: var(--muted-bg);
     border-radius: var(--radius-block);
+
+    transition: transform 0.25s ease, box-shadow 0.25s ease 0.1s;
+  }
+
+  .group:hover .img,
+  .group:focus-visible .img {
+    transform: translateY(-0.25rem);
+    box-shadow: var(--shadow);
   }
 
   .group .img img {
