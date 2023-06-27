b51cbd8e35d7644739c12e0c4af4dee214c2ee15
Ewen Le Bihan <hey@ewen.works>
2023-06-27 16:12:21 +0200

    feat: implement navigation/tabs

---
+++ packages/app/src/routes/club/[group]/event/[event]/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+  import NavigationTabs from '$lib/components/NavigationTabs.svelte';
+
+  const TABS = {
+    '': 'Infos',
+    edit: 'Modifier',
+    registrations: 'Places',
+    scan: 'Vérifier',
+  } as const;
+
+  const TABS_ORDER: Array<keyof typeof TABS> = ['', 'edit', 'registrations', 'scan'];
+
+  let pathLeaf = '';
+  $: pathLeaf = $page.url.pathname.replace(/\/$/, '').split('/').pop() || '';
+
+  let currentTab: keyof typeof TABS = '';
+  $: {
+    currentTab = pathLeaf in TABS ? pathLeaf as keyof typeof TABS : '';
+  }
+</script>
+
+<NavigationTabs
+  tabs={TABS_ORDER.map((tab) => ({
+    name: TABS[tab],
+    href: currentTab === tab ? '.' : currentTab === '' ? `./${tab}` : `../${tab}`,
+  }))}
+/>
+
+<slot />
