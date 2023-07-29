ed4ec9d06f8a84f5a60ae7638dea3c69af91e3d8
Ewen Le Bihan <hey@ewen.works>
2023-07-29 15:29:16 +0900

    build: fix build

--- packages/api/src/godchildren-tree.ts
+++ packages/api/src/godchildren-tree.ts
@@ -1,5 +1,5 @@
 import type { User } from '@prisma/client';
-import { prisma } from './prisma';
+import { prisma } from './prisma.js';
 import { GraphQLError } from 'graphql';
 
 export class FamilyTree {
