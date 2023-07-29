739e85d45065246a4ed4a4d1aa8dfbdaf25e582f
Ewen Le Bihan <hey@ewen.works>
2023-07-29 19:20:56 +0900

    feat: add button to create group on groups page

--- packages/app/src/routes/groups/+page.svelte
+++ packages/app/src/routes/groups/+page.svelte
@@ -2,13 +2,21 @@
   import type { PageData } from './$types';
   import Group from '../../lib/components/TreeGroups.svelte';
   import { createForest } from 'arborist';
+  import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
+  import IconPlus from '~icons/mdi/plus';
+  import { me } from '$lib/session';
 
   export let data: PageData;
 
   const trees = createForest(data.groups, { idKey: 'groupId' });
 </script>
 
-<h1>Groupes</h1>
+<h1>
+  Groupes {#if $me?.admin || $me?.canEditGroups}<ButtonSecondary
+      icon={IconPlus}
+      href="/clubs/create">Créer</ButtonSecondary
+    >{/if}
+</h1>
 
 <ul class="nobullet">
   {#each trees as group}
@@ -28,6 +36,10 @@
   }
 
   h1 {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
     margin-bottom: 3rem;
     text-align: center;
   }
