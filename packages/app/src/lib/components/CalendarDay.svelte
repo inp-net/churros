74d552ed12b562b3c4cb1068de1ddeba5ee7593d
Antonin Litschgy <litschan@bde.enseeiht.fr>
2023-08-01 21:59:19 +0200

    feat: calendar

---
+++ packages/app/src/lib/components/CalendarDay.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  export let date: Date;
+  const week = ['LUN', 'MAR', 'MER', 'JEU', 'VEN', 'SAM', 'DIM'];
+  const months = [
+    'Janvier',
+    'Février',
+    'Mars',
+    'Avril',
+    'Mai',
+    'Juin',
+    'Juillet',
+    'Août',
+    'Septembre',
+    'Octobre',
+    'Novembre',
+    'Décembre',
+  ];
+</script>
+
+<div class="date">
+  <span class="day-week">{week[date.getDay()]}</span>
+  <span class="day-number">{date.getDate()}</span>
+  <span class="month">{months[date.getMonth()]}</span>
+</div>
+
+<style>
+  .date {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .date > span {
+    line-height: 1em;
+    text-align: center;
+  }
+
+  .day-week {
+    font-size: 1.5em;
+    font-weight: 600;
+  }
+
+  .day-number {
+    font-size: 3em;
+    font-weight: 750;
+  }
+
+  .month {
+    font-size: 1.5em;
+    font-weight: 600;
+  }
+</style>
--- packages/app/src/routes/demo/+page.svelte
+++ packages/app/src/routes/demo/+page.svelte
@@ -1,20 +1,17 @@
 <script lang="ts">
-  import Groups from '$lib/components/CarouselGroups.svelte';
+  // PAGE DE TEST A SUPPRIMER
+  import CalendarDay from '$lib/components/CalendarDay.svelte';
 
-  const groups = [
-    { name: 'Ski', pictureFile: 'groups/ski.jpg', uid: 'ski' },
-    { name: 'Cinéma', pictureFile: 'groups/cinema.jpg', uid: 'cinema' },
-    { name: 'Pétanque', pictureFile: 'groups/petanque.jpg', uid: 'petanque' },
-    { name: 'Vélo', pictureFile: 'groups/velo.jpg', uid: 'velo' },
-    { name: 'Ski2', pictureFile: 'groups/ski.jpg', uid: 'ski' },
-    { name: 'Cinéma2', pictureFile: 'groups/cinema.jpg', uid: 'cinema' },
-    { name: 'Pétanque2', pictureFile: 'groups/petanque.jpg', uid: 'petanque' },
-    { name: 'Vélo2', pictureFile: 'groups/velo.jpg', uid: 'velo' },
-    { name: 'Ski3', pictureFile: 'groups/ski.jpg', uid: 'ski' },
-    { name: 'Cinéma3', pictureFile: 'groups/cinema.jpg', uid: 'cinema' },
-    { name: 'Pétanque3', pictureFile: 'groups/petanque.jpg', uid: 'petanque' },
-    { name: 'Vélo3', pictureFile: 'groups/velo.jpg', uid: 'velo' },
-  ];
+  let days = 0;
+
+  function incrementDays() {
+    days += 1;
+    console.log(`Days: ${days}`);
+  }
+
+  setInterval(incrementDays, 200); // 1000 ms = 1 seconde
+
+  $: date = new Date(days * 1000 * 60 * 60 * 24);
 </script>
 
-<Groups {groups} />
+<CalendarDay {date} />
