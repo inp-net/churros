2b4099c84f1027ce14970e60d17b77bc5a5f213d
Antonin Litschgy <litschan@bde.enseeiht.fr>
2023-08-06 18:16:55 +0200

    fix: suppressing demo page (shouldn't have added it)

--- packages/app/src/routes/demo/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  // PAGE DE TEST A SUPPRIMER
-  import InputToggle from '$lib/components/InputToggle.svelte';
-
-  const value = false;
-</script>
-
-<InputToggle label="Thing" {value} />
