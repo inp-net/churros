c86c0dd930f0d4891515aae791108e23fff026c0
Ewen Le Bihan <hey@ewen.works>
2023-08-08 18:12:32 +0900

    fix(birthdays page): not sorted by date

--- packages/app/src/routes/birthdays/+page.svelte
+++ packages/app/src/routes/birthdays/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import AvatarPerson from '$lib/components/AvatarPerson.svelte';
-  import { format } from 'date-fns';
+  import { format, parse } from 'date-fns';
   import type { PageData } from './$types';
   import fr from 'date-fns/locale/fr/index.js';
   import ButtonBack from '$lib/components/ButtonBack.svelte';
@@ -19,12 +19,19 @@
       else groupedByBirthday[key] = [user];
     }
   }
+  
+  const parseBackDisplayedDate = (date: string) => {
+    if (date === "Aujourd'hui") return new Date();
+    return parse(date, 'dd MMMM', new Date());
+  };
+
+  const sortWithDisplayDate = (a: string, b: string) => parseBackDisplayedDate(a).valueOf() - parseBackDisplayedDate(b).valueOf();
 </script>
 
 <h1><ButtonBack /> Anniversaires</h1>
 
 <ul class="nobullet birthdays">
-  {#each Object.entries(groupedByBirthday) as [birthday, users]}
+  {#each Object.entries(groupedByBirthday).sort( ([a, _], [b, _2]) => sortWithDisplayDate(a, b) ) as [birthday, users]}
     <li>
       <h2>{birthday}</h2>
       <ul class="nobullet">
