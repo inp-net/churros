1da70f399ce20a439b6bd9dfe045ca6f79c14043
Ewen Le Bihan <hey@ewen.works>
2023-08-09 11:29:47 +0900

    fix(registrations): disable reimbursments for now

--- packages/api/src/objects/registrations.ts
+++ packages/api/src/objects/registrations.ts
@@ -504,26 +504,31 @@ builder.mutationField('deleteRegistration', (t) =>
       return true;
     },
     async resolve(_, { id }, {}) {
-      const registration = await prisma.registration.findFirstOrThrow({
-        where: { id },
-        include: {
-          ticket: { include: { event: { include: { beneficiary: true } } } },
-          author: true,
-        },
-      });
-      if (
-        registration.paid &&
-        registration.ticket.event.beneficiary &&
-        registration.ticket.price > 0 &&
-        registration.paymentMethod
-      ) {
-        await pay(
-          registration.ticket.event.beneficiary.uid,
-          registration.author.uid,
-          registration.ticket.price,
-          registration.paymentMethod
-        );
-      }
+      // const registration = await prisma.registration.findFirstOrThrow({
+      //   where: { id },
+      //   include: {
+      //     ticket: { include: { event: { include: { beneficiary: true } } } },
+      //     author: true,
+      //   },
+      // });
+      // const beneficiaryUser = await prisma.user.findUnique({
+      //   where: {uid: registration.beneficiary || registration.author.uid}
+      // })
+      // if (
+      //   registration.paid &&
+      //   registration.ticket.event.beneficiary &&
+      //   registration.ticket.price > 0 &&
+      //   registration.paymentMethod &&
+      //   beneficiaryUser
+      // ) {
+      //   await pay(
+      //     registration.ticket.event.beneficiary.uid,
+      //     registration.author.uid,
+      //     registration.ticket.price,
+      //     registration.paymentMethod,
+      //     beneficiaryUser.phone
+      //   );
+      // }
 
       await prisma.registration.deleteMany({
         where: { id },
