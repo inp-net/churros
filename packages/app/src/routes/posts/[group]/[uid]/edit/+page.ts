162cfb608f1e43e747f39cf249524b2bf0aa1e13
Ewen Le Bihan <ewen@voyager>
2023-08-24 16:23:36 +0200

    fix(edit articles): anyone can access the page (closes #139)
    but not save edits (the API returns an error)

--- packages/app/src/routes/posts/[group]/[uid]/edit/+page.ts
+++ packages/app/src/routes/posts/[group]/[uid]/edit/+page.ts
@@ -1,4 +1,6 @@
+import { redirectToLogin } from '$lib/session';
 import { Selector, loadQuery } from '$lib/zeus.js';
+import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const _articleQuery = Selector('Article')({
@@ -39,10 +41,22 @@ export const _articleQuery = Selector('Article')({
   pictureFile: true,
 });
 
-export const load: PageLoad = async ({ fetch, params, parent }) =>
-  loadQuery(
+export const load: PageLoad = async ({ fetch, params, parent, url }) => {
+  const { me } = await parent();
+  if (!me) throw redirectToLogin(url.pathname);
+  const { article } = await loadQuery(
     {
       article: [{ uid: params.uid, groupUid: params.group }, _articleQuery],
     },
     { fetch, parent }
   );
+
+  const canEdit =
+    me.canEditGroups ||
+    (Boolean(article.author?.id === me.id) &&
+      me.groups.some(
+        ({ canEditArticles, group }) => group.id === article.group.id && canEditArticles
+      ));
+  if (!canEdit) throw redirect(307, '..');
+  return { article };
+};
