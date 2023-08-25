26fc916aabf95d13531a189e6560f45fb0f4e0ed
Ewen Le Bihan <ewen@voyager>
2023-08-26 00:09:54 +0200

    chore: fix inject-current-commit-hash

--- packages/api/scripts/inject-current-commit-hash.ts
+++ packages/api/scripts/inject-current-commit-hash.ts
@@ -1,27 +1,20 @@
-import { exec } from 'node:child_process';
+import { execa } from 'execa';
 import { readFileSync, writeFileSync } from 'node:fs';
 import path from 'node:path';
 
-exec('git rev-parse HEAD').stdout?.on('data', (hash: string) => {
-  if (hash.toString().trim() === '') {
-    return;
-  }
+async function git(args: string): Promise<string> {
+  const { stdout } = await execa('git', args.split(' '));
+  return stdout;
+}
 
-  exec('git rev-parse --show-toplevel').stdout?.on('data', (toplevel: string) => {
-    if (toplevel.toString().trim() === '') return;
-    const filepath = path.join(toplevel.trim(), './packages/app/src/lib/buildinfo.ts');
-
-    exec(
-      'git for-each-ref refs/tags --sort=-taggerdate --format=%(refname:short) --count=1 --points-at=HEAD'
-    ).stdout?.on('data', (tag: string) => {
-      console.log(`Injecting CURRENT_COMMIT="${hash.trim()}"`);
-      console.log(`Injecting CURRENT_VERSION=${tag.trim() || 'alpha'}`);
-      const content = readFileSync(filepath)
-        .toString()
-        .replace(/^CURRENT_COMMIT=.*$/m, `CURRENT_COMMIT="${hash.trim()}"`)
-        .replace(/^CURRENT_VERSION=.*$/m, `CURRENT_VERSION="${tag.trim() || 'alpha'}"`);
-
-      writeFileSync(filepath, content);
-    });
-  });
-});
+const hash = await git('rev-parse HEAD');
+const toplevel = await git('rev-parse --show-toplevel');
+const tag = await git('for-each-ref refs/tags --sort=-refname --format=%(refname:short) --count=1');
+const filepath = path.join(toplevel.trim(), './packages/app/src/lib/buildinfo.ts');
+console.log(`Injecting CURRENT_COMMIT="${hash.trim()}" into ${filepath}`);
+console.log(`Injecting CURRENT_VERSION=${tag.trim() || 'alpha'} into ${filepath}`);
+const content = readFileSync(filepath)
+  .toString()
+  .replace(/^CURRENT_COMMIT=.*$/m, `CURRENT_COMMIT="${hash.trim()}"`)
+  .replace(/^CURRENT_VERSION=.*$/m, `CURRENT_VERSION="${tag.trim() || 'alpha'}"`);
+writeFileSync(filepath, content);
