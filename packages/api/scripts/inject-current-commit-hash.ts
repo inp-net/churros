ef51a862dc616f03e6c97a98626e30fe479529db
Ewen Le Bihan <ewen@voyager>
2023-08-26 13:10:11 +0200

    fix(inject current commit): remove v prefix from version

--- packages/api/scripts/inject-current-commit-hash.ts
+++ packages/api/scripts/inject-current-commit-hash.ts
@@ -7,20 +7,22 @@ async function git(args: string): Promise<string> {
   return stdout;
 }
 
-const hash = await git('rev-parse HEAD');
+const hash = await git('rev-parse HEAD').then((hash) => hash.trim());
 const toplevel = await git('rev-parse --show-toplevel');
-const tag = await git('for-each-ref refs/tags --sort=-refname --format=%(refname:short) --count=1');
+const tag = await git(
+  'for-each-ref refs/tags --sort=-refname --format=%(refname:short) --count=1'
+).then((tag) => tag.trim().replace(/^v/, ''));
 const filepath = path.join(toplevel.trim(), './packages/app/src/lib/buildinfo.ts');
-console.log(`Injecting CURRENT_COMMIT="${hash.trim()}" into ${filepath}`);
-console.log(`Injecting CURRENT_VERSION=${tag.trim() || 'alpha'} into ${filepath}`);
+console.log(`Injecting CURRENT_COMMIT="${hash}" into ${filepath}`);
+console.log(`Injecting CURRENT_VERSION=${tag} into ${filepath}`);
 let content = readFileSync(filepath)
   .toString()
   .replace(
     /export const CURRENT_COMMIT\s*=.*$/m,
-    `export const CURRENT_COMMIT = "${hash.trim()}" as string`
+    `export const CURRENT_COMMIT = "${hash}" as string`
   )
   .replace(
     /export const CURRENT_VERSION\s*=.*$/m,
-    `export const CURRENT_VERSION = "${tag.trim() || 'alpha'}" as string`
+    `export const CURRENT_VERSION = "${tag}" as string`
   );
 writeFileSync(filepath, content);
