39f01a8f1af9ba05127307133b5f8c2506ba8f51
Ewen Le Bihan <ewen@voyager>
2023-08-26 02:28:37 +0200

    fix(buildinfo): fix injection script

--- packages/api/scripts/inject-current-commit-hash.ts
+++ packages/api/scripts/inject-current-commit-hash.ts
@@ -13,8 +13,14 @@ const tag = await git('for-each-ref refs/tags --sort=-refname --format=%(refname
 const filepath = path.join(toplevel.trim(), './packages/app/src/lib/buildinfo.ts');
 console.log(`Injecting CURRENT_COMMIT="${hash.trim()}" into ${filepath}`);
 console.log(`Injecting CURRENT_VERSION=${tag.trim() || 'alpha'} into ${filepath}`);
-const content = readFileSync(filepath)
+let content = readFileSync(filepath)
   .toString()
-  .replace(/^CURRENT_COMMIT=.*$/m, `CURRENT_COMMIT="${hash.trim()}"`)
-  .replace(/^CURRENT_VERSION=.*$/m, `CURRENT_VERSION="${tag.trim() || 'alpha'}"`);
+  .replace(
+    /export const CURRENT_COMMIT\s*=.*$/m,
+    `export const CURRENT_COMMIT = "${hash.trim()}" as string`
+  )
+  .replace(
+    /export const CURRENT_VERSION\s*=.*$/m,
+    `export const CURRENT_VERSION = "${tag.trim() || 'alpha'}" as string`
+  );
 writeFileSync(filepath, content);
