7e320842066bc59f769563123f855d57a78a2943
Ewen Le Bihan <hey@ewen.works>
2023-08-29 20:37:59 +0200

    feat(logs): redirect to login when logged out

--- packages/app/src/routes/logs/+layout.ts
+++ packages/app/src/routes/logs/+layout.ts
@@ -1,8 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutLoad } from './$types';
+import { redirectToLogin } from '$lib/session';
 
 export const load: LayoutLoad = async ({ parent }) => {
   const data = await parent();
-  if (!data.me?.admin) throw redirect(307, '/');
+  if (!data.me) throw redirectToLogin('/logs');
+  if (!data.me.admin) throw redirect(307, '/');
+
   return data;
 };
