43467d41272879e211534c7e0b9849cf27b44817
Ewen Le Bihan <hey@ewen.works>
2023-09-03 22:23:39 +0200

    fix(register): use dynamic env var

--- packages/app/src/routes/register/continue/+error.svelte
+++ packages/app/src/routes/register/continue/+error.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { PUBLIC_SUPPORT_EMAIL } from '$env/static/public';
+  import { env } from '$env/dynamic/public';
   import Alert from '$lib/components/Alert.svelte';
 </script>
 
@@ -7,6 +7,6 @@
   <h3>Cette inscription a expiré.</h3>
   <p>
     Vous pouvez <a href="..">recommencer votre inscription</a> ou
-    <a href="mailto:{PUBLIC_SUPPORT_EMAIL}">contacter le support</a>.
+    <a href="mailto:{env.PUBLIC_SUPPORT_EMAIL}">contacter le support</a>.
   </p>
 </Alert>
