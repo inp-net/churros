f63307db61cc5071b151da892ee8920d16038edc
Ewen Le Bihan <hey@ewen.works>
2023-09-03 14:29:14 +0200

    feat(logs): implement frontend for paging

--- packages/app/src/routes/logs/+page.svelte
+++ packages/app/src/routes/logs/+page.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import LogsTable from '$lib/components/LogsTable.svelte';
+  import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
+  import { zeus } from '$lib/zeus';
+  import { _pageQuery } from './+page';
 
+  let loadingMore = false;
+
+  async function loadMore() {
+    if (loadingMore) return;
+    try {
+      loadingMore = true;
+      const result = await $zeus.query({
+        logs: [
+          {
+            after: data.logs.pageInfo.endCursor,
+          },
+          _pageQuery,
+        ],
+      });
+      logs.pageInfo = result.logs.pageInfo;
+      logs.edges = [...logs.edges, ...result.logs.edges];
+    } finally {
+      loadingMore = false;
+    }
+  }
   export let data: PageData;
 
   const { logs } = data;
@@ -14,6 +37,12 @@
   </header>
 
   <LogsTable logs={logs.edges.map((e) => e.node)} />
+
+  {#if logs.pageInfo.hasNextPage}
+    <section class="load-more">
+      <ButtonSecondary on:click={loadMore} loading={loadingMore}>Charger plus</ButtonSecondary>
+    </section>
+  {/if}
 </div>
 
 <style>
