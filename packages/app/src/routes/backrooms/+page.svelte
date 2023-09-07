c95ecb2a8a4102808f2a63d81cfabfdcca3dbb5c
Ewen Le Bihan <hey@ewen.works>
2023-09-07 15:18:11 +0200

    feat: add button to switch between staging and prod (closes #381)

--- packages/app/src/routes/backrooms/+page.svelte
+++ packages/app/src/routes/backrooms/+page.svelte
@@ -1,14 +1,30 @@
 <script lang="ts">
   import IconLogs from '~icons/mdi/pulse';
+  import IconDomainSwitch from '~icons/mdi/domain-switch';
   import CardService from '$lib/components/CardService.svelte';
   import InputText from '$lib/components/InputText.svelte';
   import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
+
+  import { page } from '$app/stores';
 
   let path = '/';
 </script>
 
 <div class="content">
-  <CardService name="Logs" logoFile={IconLogs} href="/logs" />
+  <ul class="nobullet">
+    <li>
+      <CardService name="Logs" logoFile={IconLogs} href="/logs" />
+    </li>
+    <li>
+      <CardService
+        name={$page.url.hostname === 'staging-churros.inpt.fr' ? 'Go to prod' : 'Go to staging'}
+        logoFile={IconDomainSwitch}
+        href={$page.url.hostname === 'staging-churros.inpt.fr'
+          ? 'https://churros.inpt.fr'
+          : 'https://staging-churros.inpt.fr'}
+      />
+    </li>
+  </ul>
 
   <div class="goto">
     <InputText label="Goto" bind:value={path} />
@@ -17,6 +33,13 @@
 </div>
 
 <style>
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+  }
+
   .content {
     max-width: 1200px;
     margin: 0 auto;
