bc6da42c85f822344eae2d1aa48153ea11199989
Ewen Le Bihan <hey@ewen.works>
2023-09-09 01:16:51 +0200

    feat(more): link to commits at tag on version name

--- packages/app/src/routes/more/+page.svelte
+++ packages/app/src/routes/more/+page.svelte
@@ -95,8 +95,11 @@
 
   <footer>
     <code
-      >v{CURRENT_VERSION} · built against {#if CURRENT_COMMIT}<a
-          href="https://git.inpt.fr/inp-net/centraverse/-/commit/{CURRENT_COMMIT}"
+      ><a href="https://git.inpt.fr/inp-net/churros/-/commits/v{CURRENT_VERSION}"
+        >v{CURRENT_VERSION}</a
+      >
+      · built against {#if CURRENT_COMMIT}<a
+          href="https://git.inpt.fr/inp-net/churros/-/commit/{CURRENT_COMMIT}"
           >{CURRENT_COMMIT.slice(0, 8)}</a
         >
       {:else}trunk{/if}
