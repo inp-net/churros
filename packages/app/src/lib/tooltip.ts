66c384ac9df24f7aaef195ff18a299365711a614
Ewen Le Bihan <hey@ewen.works>
2023-09-08 22:27:10 +0200

    fix: tooltip does not reactively update (closes #326)

--- packages/app/src/lib/tooltip.ts
+++ packages/app/src/lib/tooltip.ts
@@ -1,27 +1,49 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import tippy from 'sveltejs-tippy';
 import xss from 'xss';
 
-export const tooltip = (
-  node: HTMLElement,
-  parameters: string | [string, number] | object | undefined
-) => {
+function props(parameters: string | [string, number] | object | undefined) {
   let content: string;
   let delay = 50;
-  if (!parameters) return;
-
-  if (typeof parameters === 'string') {
-    content = parameters;
-  } else if (Array.isArray(parameters)) {
-    [content, delay] = parameters;
-  } else {
-    // node.title = parameters.content
-    return tippy(node, parameters);
+  if (!parameters) {
+    return {
+      content: '',
+      allowHtml: false,
+      delay: [delay, 0],
+    };
   }
 
-  // node.title = content
-  return tippy(node, { content: xss(content), allowHTML: true, delay: [delay, 0] });
+  if (typeof parameters === 'string') content = parameters;
+  else if (Array.isArray(parameters)) [content, delay] = parameters;
+  else return { content: '', allowHTML: false, delay: [delay, 0], ...parameters };
+
+  return { content: xss(content), allowHTML: true, delay: [delay, 0] };
+}
+
+export const tooltip = (
+  node: HTMLElement & {
+    _tippy?: {
+      destroy: () => void;
+      setProps: (props: unknown) => void;
+    };
+  },
+  parameters: string | [string, number] | object | undefined
+) => {
+  const properties = props(parameters);
+  tippy(node, properties);
+  if (properties.content.length <= 0) node._tippy?.destroy();
+
+  return {
+    update(parameters: string | [string, number] | object | undefined) {
+      const properties = props(parameters);
+      if (!node._tippy) tippy(node, properties);
+      if (properties.content.length <= 0) node._tippy?.destroy();
+      node._tippy?.setProps(properties);
+    },
+    destroy() {
+      node._tippy?.destroy();
+    },
+  };
 };
