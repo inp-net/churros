0c9ff9a47f681b3baf1c6206fb6a474935ea0e51
Ewen Le Bihan <hey@ewen.works>
2023-09-13 18:51:08 +0200

    feat(404): custom 404 page

--- packages/app/src/routes/+error.svelte
+++ packages/app/src/routes/+error.svelte
@@ -7,6 +7,24 @@
   $: ({ error, status } = $page);
 </script>
 
+{#if status === 404}
+  <style>
+    :root {
+      --bg: #000;
+      --text: #25bf22;
+      --primary-link: #54fe54;
+    }
+
+    .page {
+      display: flex;
+    }
+
+    .layout {
+      flex: 1;
+    }
+  </style>
+{/if}
+
 {#if status === 401}
   <h1>Erreur 401</h1>
   <p>Vous n'avez pas les droits requis pour accéder à cette page.</p>
@@ -14,8 +32,10 @@
   <h1>Erreur 403</h1>
   <p>Accès interdit.</p>
 {:else if status === 404}
-  <h1>Erreur 404</h1>
-  <p>Cette page n'existe pas.</p>
+  <div class="err-404">
+    <img src="/404.png" alt="404" />
+    <p>Cette page n'existe pas.</p>
+  </div>
 {:else if error}
   <h1>Erreur {status}</h1>
   <p>{error.message}</p>
@@ -23,3 +43,30 @@
   <h1>Erreur {status}</h1>
   <p>C'est tout cassé.</p>
 {/if}
+
+<style lang="scss">
+  .err-404 {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    background: black;
+
+    img {
+      width: 100%;
+      max-width: calc(min(80vw, 50vh));
+      object-fit: contain;
+    }
+
+    p {
+      margin: 0 1rem;
+      font-family: var(--font-mono);
+      font-size: 1.5rem;
+      color: #25bf22;
+      text-align: center;
+    }
+  }
+</style>
