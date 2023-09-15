96be7ea724778a5e428b55f5f29cd9776b4f2f01
Ewen Le Bihan <hey@ewen.works>
2023-09-15 15:35:26 +0200

    feat: server-side csv export (closes #404) (closes #173)

--- packages/api/src/objects/registrations.ts
+++ packages/api/src/objects/registrations.ts
@@ -13,6 +13,7 @@ import * as qrcodeGeneratorLib from 'qr-code-generator-lib';
 import qrcode from 'qrcode';
 import { createTransport } from 'nodemailer';
 import { log } from './logs.js';
+import { yearTier } from '../date.js';
 
 const mailer = createTransport(process.env.SMTP_URL);
 
@@ -967,6 +968,144 @@ builder.mutationField('deleteRegistration', (t) =>
         },
       });
       return true;
+    },
+  })
+);
+
+builder.queryField('registrationsCsv', (t) =>
+  t.field({
+    type: 'String',
+    errors: {},
+    args: {
+      eventUid: t.arg.string(),
+      groupUid: t.arg.string(),
+    },
+    async authScopes(_, { eventUid, groupUid }, { user: me }) {
+      const event = await prisma.event.findFirstOrThrow({
+        where: {
+          uid: eventUid,
+          group: {
+            uid: groupUid,
+          },
+        },
+        include: {
+          managers: {
+            include: { user: true },
+          },
+        },
+      });
+
+      return eventManagedByUser(event, me, { canVerifyRegistrations: true });
+    },
+    async resolve(_, { eventUid, groupUid }) {
+      const registrations = await prisma.registration.findMany({
+        where: {
+          ticket: {
+            event: {
+              uid: eventUid,
+              group: {
+                uid: groupUid,
+              },
+            },
+          },
+        },
+        include: {
+          ticket: true,
+          author: {
+            include: {
+              major: true,
+              contributions: { include: { option: { include: { paysFor: true } } } },
+            },
+          },
+        },
+      });
+      let result = '';
+
+      if (registrations.length <= 0) return '';
+      const columns = [
+        'Date de réservation',
+        'Bénéficiaire',
+        'Achat par',
+        'Payée',
+        'Scannée',
+        'En opposition',
+        'Annulée',
+        'Méthode de paiement',
+        'Billet',
+        'Cotisant',
+        'Filière',
+        'Année',
+        'Promo',
+        'Code de réservation',
+        'Lien vers la place',
+      ] as const;
+
+      const humanBoolean = (b: boolean) => (b ? 'Oui' : 'Non');
+      const mapping = (
+        {
+          createdAt,
+          verifiedAt,
+          opposedAt,
+          cancelledAt,
+          paid,
+          author,
+          ticket,
+          paymentMethod,
+          id,
+          beneficiary,
+        }: (typeof registrations)[number],
+        benef:
+          | undefined
+          | null
+          | {
+              firstName: string;
+              major: { shortName: string };
+              graduationYear: number;
+              lastName: string;
+              contributions: Array<{ option: { paysFor: Array<{ uid: string | null }> } }>;
+            }
+      ) =>
+        ({
+          'Date de réservation': createdAt.toISOString(),
+          Bénéficiaire: benef ? fullName(benef) : beneficiary,
+          'Achat par': fullName(author),
+          Payée: humanBoolean(paid),
+          Scannée: humanBoolean(Boolean(verifiedAt) && paid),
+          'En opposition': humanBoolean(Boolean(opposedAt)),
+          Annulée: humanBoolean(Boolean(cancelledAt)),
+          'Méthode de paiement': paymentMethod ?? 'Inconnue',
+          Billet: ticket.name,
+          Cotisant: benef
+            ? humanBoolean(
+                benef.contributions.some(({ option: { paysFor } }) =>
+                  paysFor.some(({ uid }) => uid === 'aen7')
+                )
+              )
+            : '',
+          Filière: benef?.major.shortName ?? '',
+          Année: benef ? `${yearTier(benef.graduationYear)}A` : '',
+          Promo: benef?.graduationYear.toString() ?? '',
+          'Code de réservation': id.replace(/^r:/, '').toUpperCase(),
+          'Lien vers la place': `${process.env.FRONTEND_ORIGIN}/bookings/${id.replace(/^r:/, '')}/`,
+        } satisfies Record<(typeof columns)[number], string>);
+
+      result = columns.join(',') + '\n';
+
+      for (const reg of registrations) {
+        const benef = reg.beneficiary
+          ? await prisma.user.findUnique({
+              where: { uid: reg.beneficiary },
+              include: {
+                major: true,
+                contributions: { include: { option: { include: { paysFor: true } } } },
+              },
+            })
+          : reg.author;
+        const data = mapping(reg, benef);
+        result += columns.map((c) => data[c]).join(',') + '\r\n';
+      }
+
+      return result;
     },
   })
 );
