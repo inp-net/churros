23281f40c88b75c09942cad56f4e04ebc7223fd0
Ewen Le Bihan <hey@ewen.works>
2023-09-15 09:32:48 +0200

    chore(set deployments): add convinience with 0 args or 1 arg

--- set-deployments.sh
+++ set-deployments.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-echo Setting prod to $1
-echo Setting staging to $2
+case $# in
+    0)
+        lastTag=`git for-each-ref refs/tags --sort=-v:refname --format='%(refname:short)' --count=1`
+        latest=${lastTag#v}
+        prod=$latest
+        staging=$latest
+	;;
+    1)
+        prod=$1
+        staging=$1
+        ;;
+    2)
+        prod=$1
+        staging=$2
+        ;;
+    *)
+        echo "Usage: $0 <prod> <staging>"
+        exit 1
+        ;;
+esac
+
+
+echo Setting prod to $prod
+echo Setting staging to $staging
 
 realwd=$(pwd)
 
@@ -9,13 +31,13 @@ cd $(realpath k8s)
 
 git pull --autostash
 
-sed -i "s@harbor.k8s.inpt.fr/net7/centraverse:.*@harbor.k8s.inpt.fr/net7/centraverse:$1@g" deployment-{api,app}.yaml 
-sed -i "s@harbor.k8s.inpt.fr/net7/centraverse:.*@harbor.k8s.inpt.fr/net7/centraverse:$2@g" deployment-{api,app}-staging.yaml 
+sed -i "s@harbor.k8s.inpt.fr/net7/centraverse:.*@harbor.k8s.inpt.fr/net7/centraverse:$prod@g" deployment-{api,app}.yaml 
+sed -i "s@harbor.k8s.inpt.fr/net7/centraverse:.*@harbor.k8s.inpt.fr/net7/centraverse:$staging@g" deployment-{api,app}-staging.yaml 
 
 
 git add deployment-{api,app}{,-staging}.yaml
 
-git commit -m "churros: bump prod to $1 and staging to $2"
+git commit -m "churros: bump prod to $prod and staging to $staging"
 
 git push 
 
