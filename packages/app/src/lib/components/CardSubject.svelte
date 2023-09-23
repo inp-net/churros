58322cae856aa8795644d05f8f88b864a46a8851
Ewen Le Bihan <hey@ewen.works>
2023-09-23 23:06:19 +0200

    chore: format code

--- packages/app/src/lib/components/CardSubject.svelte
+++ packages/app/src/lib/components/CardSubject.svelte
@@ -1,58 +1,68 @@
 <script lang="ts">
-    import { formatRelative } from 'date-fns';
-    import fr from 'date-fns/locale/fr/index.js';
+  import { formatRelative } from 'date-fns';
+  import fr from 'date-fns/locale/fr/index.js';
 
-    type Minor = {
-        uid: string;
-        name: string;
-        shortName: string;
-    }
-    export let href: string
-    export let documentsCount: number;
-    export let name: string
-    export let shortName: string
-    export let majors: Array<{uid: string; name: string}>
-    export let minors: Minor[]
+  type Minor = {
+    uid: string;
+    name: string;
+    shortName: string;
+  };
+  export let href: string;
+  export let documentsCount: number;
   export let nextExamAt: Date | undefined=undefined;
+  export let name: string;
+  export let shortName: string;
+  export let majors: Array<{ uid: string; name: string }>;
+  export let minors: Minor[];
 </script>
 
-    <a {href}>
-<article class="document">
-        <header>
-            <h3>{shortName||name}</h3>
-        </header>
-        <p class="infos">
-{#if nextExamAt}
-                <span class="exam-date">Partiel {formatRelative(nextExamAt, new Date(), {
-                    locale: fr,
-                    weekStartsOn: 1,
-                })}</span> · 
-{/if}
-<span class="documents-count">{#if documentsCount}{documentsCount} document{documentsCount > 1 ? 's' :''}{:else}Aucun document{/if}</span> · <span class="majors-and-minors">
-    {[...minors, ...majors].map(m => m.name).join(', ')}
-</span>
-        </p>
-</article>
-    </a>
+<a {href}>
+  <article class="document">
+    <header>
+      <h3>{shortName || name}</h3>
+    </header>
+    <p class="infos">
+      {#if nextExamAt}
+        <span class="exam-date"
+          >Partiel <strong class="primary">
+            {formatRelative(nextExamAt, new Date(), {
+              locale: fr,
+              weekStartsOn: 1,
+            })}
+          </strong></span
+        > ·
+      {/if}
+      <span class="documents-count"
+        >{#if documentsCount}{documentsCount} document{documentsCount > 1 ? 's' : ''}{:else}Aucun
+          document{/if}</span
+      >
+      ·
+      <span class="majors-and-minors">
+        {[...minors, ...majors].map((m) => m.name).join(', ')}
+      </span>
+    </p>
+  </article>
+</a>
+
 <style lang="scss">
+  article {
+    padding: 0.5em 1em;
+    border: var(--border-block) solid var(--muted-border);
+    border-radius: 0.5em;
+  }
+
+  a:hover,
+  a:focus-visible {
     article {
-        padding: 0.5em 1em;
-        border: var(--border-block) solid var(--muted-border);
-        border-radius: 0.5em;
+      background-color: var(--hover-bg);
+      border-color: var(--border);
     }
+  }
 
-    a:hover, a:focus-visible {
-        article {
-            background-color: var(--hover-bg);
-            border-color: var(--border);
-        }
-    }
-
-    .infos {
-        display: flex;
-        flex-wrap: wrap;
-        column-gap: 0.5em;
-        align-items: center;
-    }
-
+  .infos {
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 0.5em;
+    align-items: center;
+  }
 </style>
