2a5e71feb4bb6a56e0c5dff303a4f68f0566470d
Ewen Le Bihan <hey@ewen.works>
2023-09-30 14:52:57 +0200

    chore: setup CLI code formatting

--- cli/churros/cli.py
+++ cli/churros/cli.py
@@ -30,6 +30,7 @@ from gql.transport.exceptions import TransportQueryError
 transport = AIOHTTPTransport(url="https://churros.inpt.fr/graphql")
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
+
 def get_token():
     cache_filepath = Path.home() / ".cache/churros.json"
     if cache_filepath.exists():
@@ -37,13 +38,17 @@ def get_token():
     else:
         return None
 
-def graphql(query: str, params: dict[str, Any]|None = None):
-    if (token := get_token()):
+
+def graphql(query: str, params: dict[str, Any] | None = None):
+    if token := get_token():
         transport.headers = {"Authorization": f"Bearer {token}"}
     else:
         transport.headers = {}
-    
-    return json_camelcase_to_snakecase(client.execute(gql(query), variable_values=params))
+
+    return json_camelcase_to_snakecase(
+        client.execute(gql(query), variable_values=params)
+    )
+
 
 class Major(NamedTuple):
     name: str
@@ -51,9 +56,10 @@ class Major(NamedTuple):
     uid: str
 
     @staticmethod
-    def gql_fields(outer = False) -> str:
+    def gql_fields(outer=False) -> str:
         fields = "name, shortName, uid"
         return f"{{ {fields} }}" if outer else fields
+
 
 class User(NamedTuple):
     full_name: str
@@ -66,56 +72,68 @@ class User(NamedTuple):
     address: str
     birthday: str
     graduation_year: int
-    major: Major 
+    major: Major
 
     @staticmethod
-    def gql_fields(outer = False) -> str:
+    def gql_fields(outer=False) -> str:
         fields = f"fullName, uid, email, firstName, lastName, otherEmails, phone, address, birthday, major {Major.gql_fields(True)}, graduationYear"
         return f"{{ {fields} }}" if outer else fields
 
+
 def json_camelcase_to_snakecase(json: dict[str, Any]) -> dict[str, Any]:
     return {
-        re.sub(r'(?<!^)(?=[A-Z])', '_', key).lower(): json_camelcase_to_snakecase(value) if isinstance(value, dict) else value
+        re.sub(r"(?<!^)(?=[A-Z])", "_", key).lower(): json_camelcase_to_snakecase(value)
+        if isinstance(value, dict)
+        else value
         for key, value in json.items()
     }
 
 
-
 def me() -> User:
-    me = graphql("query { me {"+User.gql_fields()+"} }")["me"]
-    
+    me = graphql("query { me {" + User.gql_fields() + "} }")["me"]
+
     return User(**me)
 
+
 def user(uid: str) -> User:
-    u = graphql("query user($uid: String!) { user(uid: $uid) { "+User.gql_fields()+" } }", {"uid": uid})["user"]
-    
+    u = graphql(
+        "query user($uid: String!) { user(uid: $uid) { " + User.gql_fields() + " } }",
+        {"uid": uid},
+    )["user"]
+
     return User(**u)
 
-    
 
 def run():
     opts = docopt(__doc__)
 
-
     def output(data: Any, message: str):
-        if opts['--json']:
+        if opts["--json"]:
             print(json.dumps(data, indent=4, ensure_ascii=False))
-        elif opts['--csv']:
+        elif opts["--csv"]:
             if isinstance(data, dict):
                 data = [data]
             # print header
             print(",".join(data[0].keys()))
             # print values
             for line in data:
-                print(",".join([' '.join(v) if isinstance(v, list) else f"{v}" for v in  line.values()]))
+                print(
+                    ",".join(
+                        [
+                            " ".join(v) if isinstance(v, list) else f"{v}"
+                            for v in line.values()
+                        ]
+                    )
+                )
         else:
             print(message)
 
-    if opts['login']:
-        username = opts['<username>'] 
-        password = opts['<password>'] or getpass.getpass("Password: ")
+    if opts["login"]:
+        username = opts["<username>"]
+        password = opts["<password>"] or getpass.getpass("Password: ")
 
-        query = gql("""
+        query = gql(
+            """
             mutation login($username: String!, $password: String!) {
                 login(email: $username, password: $password) {
                     __typename
@@ -129,71 +147,95 @@ def run():
                     }
                 }
             }
-        """)
+        """
+        )
 
-        params = {
-            "username": username,
-            "password": password
-        }
+        params = {"username": username, "password": password}
 
         result = client.execute(query, variable_values=params).get("login")
 
-        if result is None: 
+        if result is None:
             print("Server error")
             return 1
-        
+
         if result.get("__typename") == "Error":
             print(result.get("message"))
             return 1
-        
+
         token = result.get("data").get("token")
 
         cache_filepath = Path.home() / ".cache/churros.json"
         cache_filepath.parent.mkdir(parents=True, exist_ok=True)
-        cache_filepath.write_text(json.dumps({"token": token}, indent=4, ensure_ascii=False))
+        cache_filepath.write_text(
+            json.dumps({"token": token}, indent=4, ensure_ascii=False)
+        )
 
-        output({"token": token, "cache_filepath": cache_filepath}, f"Logged in successfully. Token cached to {cache_filepath}.")
-    
-    elif opts['logout']:
+        output(
+            {"token": token, "cache_filepath": cache_filepath},
+            f"Logged in successfully. Token cached to {cache_filepath}.",
+        )
+
+    elif opts["logout"]:
         cache_filepath = Path.home() / ".cache/churros.json"
         cache_filepath.unlink(missing_ok=True)
         output({"result": "OK"}, "Logged out successfully.")
-    
-    elif opts['me']:
+
+    elif opts["me"]:
         u = me()
-        output(u._asdict(),
-f"""\
+        output(
+            u._asdict(),
+            f"""\
 {u.full_name}
 @{u.uid}
 
 {' '.join([u.email] + u.other_emails)}
 {u.phone}
 {u.address}
-""")
-    elif opts['user']:
-        u = user(opts['<uid>'])
-        output(u._asdict(), 
-f"""\
+""",
+        )
+    elif opts["user"]:
+        u = user(opts["<uid>"])
+        output(
+            u._asdict(),
+            f"""\
 {u.full_name}
 @{u.uid}
 
 {' '.join([u.email] + u.other_emails)}
 {u.phone}
 {u.address}
-""")
+""",
+        )
 
     elif opts["users"]:
         lines = list(stdin.read().splitlines())
-        uids = opts['<uids>'] or [l.strip() for l in lines if l.strip()] 
+        uids = opts["<uids>"] or [l.strip() for l in lines if l.strip()]
         users = []
         for uid in uids:
             try:
                 u = user(uid)._asdict()
             except TransportQueryError as e:
-                u = {"uid": uid, "error": e.errors[0]['message']}
+                u = {"uid": uid, "error": e.errors[0]["message"]}
             users.append(u)
-        if opts['--csv']:
+        if opts["--csv"]:
             # Simplify data for CSV output
-            users = [ { k: u.get(k, '') for k in ["uid", "error", "first_name", "last_name", "graduation_year" ] } | {"major.short_name": u.get("major", {"short_name": ""}).get("short_name", "")} for u in users ]
+            users = [
+                {
+                    k: u.get(k, "")
+                    for k in [
+                        "uid",
+                        "error",
+                        "first_name",
+                        "last_name",
+                        "graduation_year",
+                    ]
+                }
+                | {
+                    "major.short_name": u.get("major", {"short_name": ""}).get(
+                        "short_name", ""
+                    )
+                }
+                for u in users
+            ]
             pass
         output(users, "Use --json or --csv")
