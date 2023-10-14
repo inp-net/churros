d81bbd08aa29ffb09e5cba8629d5737a2b3f9434
Ewen Le Bihan <hey@ewen.works>
2023-10-14 19:28:52 +0200

    chore(prisma): consolidate migrations

--- packages/api/prisma/migrations/20231014132433_improve_frappe/migration.sql
+++ packages/api/prisma/migrations/20231014132433_improve_frappe/migration.sql
@@ -1,3 +1,17 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[uid]` on the table `Major` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[uid,yearTier]` on the table `Minor` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[uid,yearTier,forApprentices]` on the table `Subject` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX "Minor_uid_key";
+
+-- DropIndex
+DROP INDEX "Subject_uid_key";
+
 -- AlterTable
 ALTER TABLE "Announcement" ALTER COLUMN "id" SET DEFAULT nanoid('ann:');
 
@@ -20,7 +34,8 @@ ALTER TABLE "ContributionOption" ALTER COLUMN "id" SET DEFAULT nanoid('contribut
 ALTER TABLE "Credential" ALTER COLUMN "id" SET DEFAULT nanoid('credential:');
 
 -- AlterTable
-ALTER TABLE "Document" ALTER COLUMN "id" SET DEFAULT nanoid('doc:');
+ALTER TABLE "Document" ALTER COLUMN "id" SET DEFAULT nanoid('doc:'),
+ALTER COLUMN "subjectId" DROP NOT NULL;
 
 -- AlterTable
 ALTER TABLE "EmailChange" ALTER COLUMN "id" SET DEFAULT nanoid('emailchange:');
@@ -47,22 +62,24 @@ ALTER TABLE "LydiaAccount" ALTER COLUMN "id" SET DEFAULT nanoid('lydia:');
 ALTER TABLE "LydiaTransaction" ALTER COLUMN "id" SET DEFAULT nanoid('lydiapayment:');
 
 -- AlterTable
-ALTER TABLE "Major" ALTER COLUMN "id" SET DEFAULT nanoid('major:');
+ALTER TABLE "Major" ALTER COLUMN "id" SET DEFAULT nanoid('major:'),
+ALTER COLUMN "uid" DROP DEFAULT;
 
 -- AlterTable
-ALTER TABLE "Minor" ALTER COLUMN "id" SET DEFAULT nanoid('minor:');
+ALTER TABLE "Minor" ADD COLUMN     "shortName" TEXT NOT NULL DEFAULT '',
+ALTER COLUMN "id" SET DEFAULT nanoid('minor:');
 
 -- AlterTable
 ALTER TABLE "Notification" ALTER COLUMN "id" SET DEFAULT nanoid('notif:');
-
--- AlterTable
-ALTER TABLE "NotificationSetting" ALTER COLUMN "id" SET DEFAULT nanoid('notifsetting:');
 
 -- AlterTable
 ALTER TABLE "NotificationSubscription" ALTER COLUMN "id" SET DEFAULT nanoid('notifsub:');
 
 -- AlterTable
 ALTER TABLE "PasswordReset" ALTER COLUMN "id" SET DEFAULT nanoid('passreset:');
+
+-- AlterTable
+ALTER TABLE "Reaction" ALTER COLUMN "id" SET DEFAULT nanoid('reac:');
 
 -- AlterTable
 ALTER TABLE "Registration" ALTER COLUMN "id" SET DEFAULT nanoid('r:');
@@ -77,7 +94,11 @@ ALTER TABLE "Service" ALTER COLUMN "id" SET DEFAULT nanoid('service:');
 ALTER TABLE "StudentAssociation" ALTER COLUMN "id" SET DEFAULT nanoid('ae:');
 
 -- AlterTable
-ALTER TABLE "Subject" ADD COLUMN     "yearTier" INTEGER,
+ALTER TABLE "Subject" ADD COLUMN     "forApprentices" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "moodleId" VARCHAR(255),
+ADD COLUMN     "semester" INTEGER,
+ADD COLUMN     "unitId" TEXT,
+ADD COLUMN     "yearTier" INTEGER,
 ALTER COLUMN "id" SET DEFAULT nanoid('subj:');
 
 -- AlterTable
@@ -91,3 +112,25 @@ ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT nanoid('u:');
 
 -- AlterTable
 ALTER TABLE "UserCandidate" ALTER COLUMN "id" SET DEFAULT nanoid('candidate:');
+
+-- CreateTable
+CREATE TABLE "TeachingUnit" (
+    "id" TEXT NOT NULL DEFAULT nanoid('ue:'),
+    "name" VARCHAR(255) NOT NULL,
+    "shortName" VARCHAR(255) NOT NULL DEFAULT '',
+    "moodleId" VARCHAR(255),
+
+    CONSTRAINT "TeachingUnit_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Major_uid_key" ON "Major"("uid");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Minor_uid_yearTier_key" ON "Minor"("uid", "yearTier");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subject_uid_yearTier_forApprentices_key" ON "Subject"("uid", "yearTier", "forApprentices");
+
+-- AddForeignKey
+ALTER TABLE "Subject" ADD CONSTRAINT "Subject_unitId_fkey" FOREIGN KEY ("unitId") REFERENCES "TeachingUnit"("id") ON DELETE SET NULL ON UPDATE CASCADE;
