ef8357f11ebe570962c7e2827e21247aa383f9aa
Ewen Le Bihan <hey@ewen.works>
2023-10-18 00:18:52 +0200

    chore: add script to set schoolUid to our ldap's loginTP on imported users (closes #607)

---
+++ packages/api/scripts/fix-school-uids.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+import { readFileSync } from 'node:fs';
+import type { LDAPTypes } from './ldap-types';
+
+const p = new PrismaClient();
+
+const { users } = JSON.parse(readFileSync('./dump-ldap.json', 'utf-8')) as LDAPTypes;
+
+for (const { uid, loginTP } of users) {
+  console.log(`${uid} => ${loginTP}`);
+  await p.user.update({
+    where: { uid },
+    data: {
+      schoolUid: loginTP,
+    },
+  });
+}
