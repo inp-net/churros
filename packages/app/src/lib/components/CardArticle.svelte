inp-net/churros

commit e0491bec6e08f80d5a6fca522c3fbf7cf0dca2e0

fix: pp des auteurs de posts cassées (closes #628)

packages/app/src/lib/components/CardArticle.svelte
@@ -53,7 +53,7 @@
   <a {href} class="post-link">
     <article class="post">
       {#if authorSrc}
-        <img src={authorSrc} alt={group.name} class="group-logo" />
+        <img src="{env.PUBLIC_STORAGE_URL}{authorSrc}" alt={group.name} class="group-logo" />
       {:else}
         <div class="group-logo no-logo"></div>
       {/if}

commit 5e57cc36ee7032c0bd64bae9236a2d9fe31dc5a2

fix(article card): group logos broke (regression from e0491bec)

packages/app/src/lib/components/CardArticle.svelte
@@ -41,7 +41,11 @@
     undefined;
   export let hideGroup = false;
 
-  $: authorSrc = hideGroup ? author?.pictureFile ?? '' : groupLogoSrc($isDark, group);
+  $: authorSrc = hideGroup
+    ? author
+      ? `${env.PUBLIC_STORAGE_URL}${author?.pictureFile}`
+      : ''
+    : groupLogoSrc($isDark, group);
 </script>
 
 <div class="post-outer" class:future={isFuture(publishedAt)}>
@@ -53,7 +57,7 @@
   <a {href} class="post-link">
     <article class="post">
       {#if authorSrc}
-        <img src="{env.PUBLIC_STORAGE_URL}{authorSrc}" alt={group.name} class="group-logo" />
+        <img src={authorSrc} alt={group.name} class="group-logo" />
       {:else}
         <div class="group-logo no-logo"></div>
       {/if}
@@ -216,6 +220,7 @@
     margin: 0.5rem 0;
     overflow: hidden;
     color: white;
+    background-position: center;
     background-size: cover;
     border-radius: var(--radius-block);
   }