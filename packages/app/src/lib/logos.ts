543b91e24f0652dc13bc015d169d37f6d79ce7d1
Ewen Le Bihan <hey@ewen.works>
2023-11-13 19:31:42 +0100

    docs(groupLogoSrc): document the function
    cuz the double slash prefix behavior is a bit weird

--- packages/app/src/lib/logos.ts
+++ packages/app/src/lib/logos.ts
@@ -1,5 +1,11 @@
 import { env } from '$env/dynamic/public';
 
+/**
+ * Get the complete path to the group's picture
+ * @param $isDark Are we in a dark theme?
+ * @param group The group's picture files. If a picture file starts with a double slash, the path will not be prefixed with PUBLIC_STORAGE_URL
+ * @returns The complete path to the group's picture, ready to use as an src on an <img> tag
+ */
 export function groupLogoSrc(
   $isDark: boolean,
   group: { pictureFile: string; pictureFileDark: string },
