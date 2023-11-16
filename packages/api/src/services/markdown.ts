b3d57e3e4fb9d4426ad3a5f8f579f70e568ba55c
Ewen Le Bihan <hey@ewen.works>
2023-11-16 13:40:01 +0100

    feat(markdown): linkify URLs, and link @mentions (see #676)

--- packages/api/src/services/markdown.ts
+++ packages/api/src/services/markdown.ts
@@ -8,39 +8,45 @@ import remarkBreaks from 'remark-breaks';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import rehypeHighlight from 'rehype-highlight';
+import linkifyHtml from 'linkify-html';
+import 'linkify-plugin-mention';
 
 /** Converts markdown to HTML. */
 export const toHtml = async (body: string) =>
-  unified()
-    .use(remarkParse)
-    .use(remarkMath)
-    .use(remarkBreaks)
-    // Downlevel titles (h1 -> h3)
-    .use(() => ({ children }) => {
-      for (const child of children)
-        if (child.type === 'heading') child.depth = Math.min(child.depth + 2, 6) as 3 | 4 | 5 | 6;
-    })
-    .use(remarkRehype)
-    .use(rehypeSanitize, {
-      ...defaultSchema,
+  linkifyHtml(
+    await unified()
+      .use(remarkParse)
+      .use(remarkMath)
+      .use(remarkBreaks)
+      // Downlevel titles (h1 -> h3)
+      .use(() => ({ children }) => {
+        for (const child of children)
+          if (child.type === 'heading') child.depth = Math.min(child.depth + 2, 6) as 3 | 4 | 5 | 6;
+      })
+      .use(remarkRehype)
+      .use(rehypeSanitize, {
+        ...defaultSchema,
+        attributes: {
+          ...defaultSchema.attributes,
+          code: [['className', /^language-./, 'math-inline', 'math-display']],
+          span: [...(defaultSchema.attributes?.['span'] || []), ['className', /^hljs-./]],
+        },
+      })
+      .use(rehypeKatex)
+      .use(rehypeHighlight)
+      .use(rehypeStringify)
+      .process(body)
+      .then(String),
+    {
+      defaultProtocol: 'https',
       attributes: {
-        ...defaultSchema.attributes,
-        code: [['className', /^language-./, 'math-inline', 'math-display']],
-        span: [...(defaultSchema.attributes?.['span'] || []), ['className', /^hljs-./]],
+        'data-external-link': 'data-external-link',
       },
-    })
-    .use(rehypeKatex)
-    .use(rehypeHighlight)
-    .use(rehypeStringify)
-    .process(body)
-    .then(String);
-// .then((s) =>
-//   s.replaceAll(
-//     /(^|\b|\s)@(\w+)(\b|\s|$)/g,
-//     (_, before: string, uid: string, after: string) =>
-//       `${before}<a href="/users/${uid}" class="user-mention">@${uid}</a>${after}`
-//   )
-// );
+      formatHref: {
+        mention: (username) => `https://churros.inpt.fr/@${username.replace(/^\//, '')}`,
+      },
+    },
+  );
 
 export function htmlToText(body: string): string {
   return convertHtmlToText(body, {
