1144002b869f1f08ce5101b5eeef9dfee56745aa
whidix <theodor.vallier@gmail.com>
2023-11-18 18:14:27 +0100

    wip: shop

--- packages/api/src/modules/curriculum/types/shop-payment.ts
+++ packages/api/src/modules/curriculum/types/shop-payment.ts
@@ -1,10 +1,17 @@
 import { builder } from '../builder.js';
 import { DateTimeScalar } from './scalars.js';
 import { prisma } from '../prisma.js';
-import { payEventRegistrationViaLydia } from '../services/lydia.js';
+import { payShopPaymentViaLydia } from '../services/lydia.js';
 import { GraphQLError } from 'graphql';
 import { PaymentMethodEnum } from './registrations.js';
-import type { PaymentMethod as PaymentMethodPrisma } from '@prisma/client';
+import type {
+  Group,
+  LydiaAccount,
+  LydiaTransaction,
+  PaymentMethod as PaymentMethodPrisma,
+  ShopItem,
+  ShopPayment,
+} from '@prisma/client';
 
 export const ShopPaymentType = builder.prismaObject('ShopPayment', {
   fields: (t) => ({
@@ -45,6 +52,9 @@ builder.queryField('orders', (t) =>
             },
           },
         },
+        orderBy: {
+          createdAt: 'desc',
+        },
       });
     },
   }),
@@ -59,16 +69,9 @@ builder.mutationField('upsertShopPayment', (t) =>
       userUid: t.arg.string({ required: true }),
       shopItemId: t.arg.string({ required: true }),
       quantity: t.arg.int({ required: true }),
-      paymentMethod: t.arg.string({ required: true }),
-      paid: t.arg.boolean({ required: true }),
-      phone: t.arg.string(),
+      paymentMethod: t.arg.string({ required: false }),
     },
-    async resolve(
-      query,
-      _,
-      { id, userUid, shopItemId, quantity, paymentMethod, paid, phone },
-      { user },
-    ) {
+    async resolve(query, _, { id, userUid, shopItemId, quantity, paymentMethod }, { user }) {
       const shopItem = await prisma.shopItem.findUniqueOrThrow({
         where: { id: shopItemId },
         include: {
@@ -76,6 +79,12 @@ builder.mutationField('upsertShopPayment', (t) =>
           lydiaAccount: true,
         },
       });
+
+      if (shopItem.stock < quantity) {
+        throw new GraphQLError('Not enough stock');
+      } else if (shopItem.max < quantity) {
+        throw new GraphQLError('Too much quantity');
+      }
 
       const shopPayment = await prisma.shopPayment.upsert({
         ...query,
@@ -85,31 +94,21 @@ builder.mutationField('upsertShopPayment', (t) =>
           shopItem: { connect: { id: shopItem.id } },
           quantity,
           paymentMethod: paymentMethod as PaymentMethodPrisma,
-          paid,
         },
         update: {
           user: { connect: { uid: userUid } },
           shopItem: { connect: { id: shopItem.id } },
           quantity,
           paymentMethod: paymentMethod as PaymentMethodPrisma,
-          paid,
         },
       });
-
-      await pay(
-        userUid,
-        shopItem.group.name,
-        quantity * shopItem.price,
-        paymentMethod as PaymentMethodPrisma,
-        phone,
-      );
 
       await prisma.logEntry.create({
         data: {
           area: 'shop payment',
           action: 'create',
           target: shopPayment.id,
-          message: `${shopItem.name} (x${quantity}) for ${userUid} ${paid ? 'paid' : 'not paid'}`,
+          message: `${shopItem.name} (x${quantity}) for ${userUid}`,
           user: user ? { connect: { id: user.id } } : undefined,
         },
       });
@@ -119,25 +118,99 @@ builder.mutationField('upsertShopPayment', (t) =>
   }),
 );
 
+builder.mutationField('paidShopPayment', (t) =>
+  t.prismaField({
+    type: ShopPaymentType,
+    errors: {},
+    args: {
+      shopPaymentId: t.arg.id(),
+      paymentMethod: t.arg({ type: PaymentMethodEnum, required: false }),
+      phone: t.arg.string({ required: false }),
+    },
+    async authScopes(_, { shopPaymentId }, { user }) {
+      const creating = !shopPaymentId;
+      if (!user) return false;
+      const shopPayment = await prisma.shopPayment.findUnique({
+        where: { id: shopPaymentId },
+        include: {
+          shopItem: {
+            include: {
+              group: true,
+              lydiaAccount: true,
+            },
+          },
+        },
+      });
+      if (!shopPayment) throw new GraphQLError("La commande n'existe pas");
+
+      if (creating) {
+        if (shopPayment.quantity > shopPayment.shopItem.stock) {
+          throw new GraphQLError('Not enough stock');
+        }
+      }
+
+      return true;
+    },
+    async resolve(query, _, { shopPaymentId, paymentMethod, phone }, { user }) {
+      if (!user) throw new GraphQLError('User not found');
+
+      const shopPayment = await prisma.shopPayment.findUnique({
+        where: { id: shopPaymentId },
+        include: {
+          shopItem: { include: { group: true, lydiaAccount: true } },
+          lydiaTransaction: true,
+        },
+      });
+      if (!shopPayment) throw new GraphQLError('Shop payment not found');
+      if (!paymentMethod) throw new GraphQLError('Payment method not found');
+      if (!phone) throw new GraphQLError('Phone not found');
+
+      // Process payment
+      await pay(user.uid, paymentMethod, shopPayment, phone);
+      await prisma.logEntry.create({
+        data: {
+          area: 'shop payment',
+          action: 'update',
+          target: shopPaymentId,
+          message: `${shopPayment.shopItem.name} (x${shopPayment.quantity}) for ${user.uid}`,
+          user: { connect: { id: user.id } },
+        },
+      });
+      return prisma.shopPayment.update({
+        ...query,
+        where: { id: shopPaymentId },
+        data: {
+          paymentMethod,
+        },
+      });
+    },
+  }),
+);
+
 async function pay(
   from: string,
-  to: string,
-  amount: number,
   by: PaymentMethodPrisma,
+  shopPayment: ShopPayment & {
+    shopItem: ShopItem & { lydiaAccount: LydiaAccount | null; group: Group };
+    lydiaTransaction: LydiaTransaction | null;
+  },
   phone?: string,
-  registrationId?: string,
 ) {
   switch (by) {
     case 'Lydia': {
       if (!phone) throw new GraphQLError('Missing phone number');
-      await payEventRegistrationViaLydia(phone, registrationId);
+      await payShopPaymentViaLydia(phone, shopPayment);
       return;
     }
 
     default: {
       return new Promise((_resolve, reject) => {
         reject(
-          new GraphQLError(`Attempt to pay ${to} ${amount} from ${from} by ${by}: not implemented`),
+          new GraphQLError(
+            `Attempt to pay ${shopPayment.shopItem.groupId} ${
+              shopPayment.shopItem.price * shopPayment.quantity
+            } from ${from} by ${by}: not implemented`,
+          ),
         );
       });
     }
--- packages/app/src/routes/groups/[uid]/shop/[itemUid]/+page.svelte
+++ packages/app/src/routes/groups/[uid]/shop/[itemUid]/+page.svelte
@@ -2,17 +2,80 @@
   import type { PageData } from './$types';
   import { onDestroy, onMount } from 'svelte';
   import { toasts } from '$lib/toasts';
+  import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
+  import { DISPLAY_PAYMENT_METHODS, PAYMENT_METHODS_ICONS } from '$lib/display';
+  import { PaymentMethod, zeus } from '$lib/zeus';
+  import { me } from '$lib/session';
+  import { goto } from '$app/navigation';
+  import InputText from '$lib/components/InputText.svelte';
   import ButtonPrimary from '$lib/components/ButtonPrimary.svelte';
-  import InputNumber from '$lib/components/InputNumber.svelte';
 
   export let data: PageData;
 
   const { shopItem } = data;
 
   let warningToastId: string;
+  let serverError = '';
+  let paying = false;
+  let paymentLoading = false;
+  let shopPaymentId = '';
+  let phone = '';
+  let choosingPaymentMethodLoading: PaymentMethod | undefined = undefined;
 
   let quantity = 1;
   let max = Math.min(shopItem.max, shopItem.stock);
+
+  async function payBy(method: PaymentMethod | undefined) {
+    if ($me?.uid === undefined) {
+      await goto('/login');
+    }
+    choosingPaymentMethodLoading = method ?? PaymentMethod.Other;
+    const { upsertShopPayment } = await $zeus.mutate({
+      upsertShopPayment: [
+        {
+          id: undefined,
+          paymentMethod: method,
+          userUid: $me?.uid ?? '',
+          shopItemId: shopItem.id,
+          quantity,
+        },
+        {
+          __typename: true,
+          '...on Error': {
+            message: true,
+          },
+          '...on MutationUpsertShopPaymentSuccess': {
+            data: {
+              id: true,
+              paid: true,
+            },
+          },
+        },
+      ],
+    });
+    choosingPaymentMethodLoading = undefined;
+
+    if (upsertShopPayment.__typename === 'Error') {
+      serverError = upsertShopPayment.message;
+      return;
+    }
+
+    serverError = '';
+
+    // TODO handle actually going there only when payment has gone through
+    if (method === PaymentMethod.Lydia) {
+      shopPaymentId = upsertShopPayment.data.id;
+      paying = true;
+    } else {
+      await goto(
+        '?' +
+          new URLSearchParams({
+            done: upsertShopPayment.data.id,
+            ...(upsertShopPayment.data.paid ? { paid: '' } : {}),
+          }).toString(),
+      );
+    }
+  }
 
   onMount(() => {
     warningToastId = toasts.warn('Page en bêta', 'Les boutiques sont en cours de fonctionnement', {
@@ -31,5 +94,68 @@
   <p>max: {shopItem.max}</p>
   <p>{shopItem.description}</p>
   <input type="number" bind:value={quantity} min="1" {max} />
-  <ButtonPrimary>Acheter</ButtonPrimary>
+
+  <ul class="nobullet payment-methods">
+    {#if !paying}
+      <ul class="nobullet payment-methods">
+        {#each shopItem.paymentMethods as method}
+          <li>
+            <ButtonSecondary
+              loading={choosingPaymentMethodLoading === method}
+              disabled={Boolean(choosingPaymentMethodLoading)}
+              icon={PAYMENT_METHODS_ICONS[method]}
+              on:click={async () => payBy(method)}
+            >
+              {DISPLAY_PAYMENT_METHODS[method]}
+            </ButtonSecondary>
+          </li>
+        {:else}
+          <li class="no-payment-methods danger">
+            Aucun moyen de paiement disponible. Contactez les managers de l'évènement.
+          </li>
+        {/each}
+      </ul>
+    {:else}
+      <form
+        class="pay"
+        on:submit|preventDefault={async () => {
+          paymentLoading = true;
+          const { paidShopPayment } = await $zeus.mutate({
+            paidShopPayment: [
+              {
+                shopPaymentId: shopPaymentId,
+                phone: phone,
+                paymentMethod: PaymentMethod.Lydia,
+              },
+              {
+                __typename: true,
+                '...on Error': { message: true },
+                '...on MutationPaidShopPaymentSuccess': {
+                  data: {
+                    __typename: true,
+                  },
+                },
+              },
+            ],
+          });
+          if (paidShopPayment.__typename === 'Error') serverError = paidShopPayment.message;
+          else await goto('?' + new URLSearchParams({ done: shopPaymentId }).toString());
+        }}
+      >
+        <InputText
+          type="tel"
+          label="Numéro de téléphone"
+          initial={$me?.phone}
+          maxlength={255}
+          bind:value={phone}
+        />
+
+        <section class="submit">
+          <ButtonPrimary loading={paymentLoading} submits
+            >Payer {quantity * shopItem.price}€</ButtonPrimary
+          >
+        </section>
+      </form>
+    {/if}
+  </ul>
 </div>
