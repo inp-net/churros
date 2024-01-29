8fd4e0bbf7ab8469d9bc302b44382b49df231cec
Hugo Simon <hugo.simon@etu.inp-n7.fr>
2024-01-29 18:57:47 +0100

    feat(services): backroom page front evolved and new service button

--- packages/app/src/routes/(app)/backrooms/services/+page.svelte
+++ packages/app/src/routes/(app)/backrooms/services/+page.svelte
@@ -1,46 +1,73 @@
 <script lang="ts">
-    import CardService from '$lib/components/CardService.svelte';
-    import type { PageData } from './$types';
+  import CardService from '$lib/components/CardService.svelte';
+  import IconGear from '~icons/mdi/gear-outline';
+  import type { PageData } from './$types';
 
-    export let data: PageData;
+  export let data: PageData;
 
-    const { services } = data;
+  const { services } = data;
 </script>
 
 <div class="content">
-    <h1> Gérer les services </h1>
+  <h1>Gérer les services</h1>
 
-    <ul class="nobullet">
-        {#each services as service}
-            <li>
-                <CardService service={{...service, url:`/services/${service?.id}/edit/`}} />
-            </li>
-        {/each}
-    </ul>
+  <ul class="nobullet">
+    {#each services as service}
+      <li>
+        <a class="edit-icon" href="/services/{service?.id}/edit/">
+          <IconGear />
+        </a>
+        <CardService service={{ ...service, url: `/services/${service?.id}/edit/` }} />
+      </li>
+    {/each}
+    <li class="new">
+      <CardService
+        service={{
+          name: 'Ajouter un service',
+          logo: 'add',
+          logoSourceType: 'Icon',
+
+          url: '/services/create',
+        }}
+        dashedBorder
+      />
+    </li>
+  </ul>
 </div>
-      
-<style>
-    .content {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 100%;
-    }
 
-    h1 {
-        margin-bottom: 2rem;
-        text-align: center;
-    }
+<style lang="scss">
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+  }
 
-    ul {
-        display: flex;
-        flex-basis: 100%;
-        gap: 1rem;
-        justify-content: center;
-    }
+  .edit-icon {
+    position: absolute;
+    display: none;
+    transform: translate(8rem, 0.5rem);
+  }
 
-    li:hover,
-    li:focus-visible {
-        cursor: pointer;
-        }
+  h1 {
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 1rem;
+    justify-content: center;
+  }
+
+  li:hover,
+  li:focus-visible {
+    cursor: pointer;
+
+    .edit-icon {
+      display: block;
+      color: var(--text);
+    }
+  }
 </style>
