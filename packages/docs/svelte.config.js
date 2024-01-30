3b8c011df06a6572b4d94dcde4ae169a3a22eade
Ewen Le Bihan <hey@ewen.works>
2024-01-30 01:26:25 +0100

    chore(docs): try to fix CI (round 6)

--- packages/docs/svelte.config.js
+++ packages/docs/svelte.config.js
@@ -17,7 +17,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		prerender: {
-			handleMissingId: 'ignore'
+			handleMissingId: 'ignore',
+			handleHttpError: 'warn'
 		}
 	}
 };
