5f47cdae4f9cb244db008e2ed54bf243117adc44
Ewen Le Bihan <hey@ewen.works>
2024-01-31 15:27:15 +0100

    WIP: reorganize logs module

--- packages/api/src/modules/logs/resolvers/query.logs.ts
+++ packages/api/src/modules/logs/resolvers/query.logs.ts
@@ -1,1 +1,27 @@
-// from old.ts
+import { builder, prisma } from '#lib';
+import {} from '#modules/global';
+import { GraphQLError } from 'graphql';
+import { LogType } from '../index.js';
+
+builder.queryField('logs', (t) =>
+  t.prismaConnection({
+    type: LogType,
+    cursor: 'id',
+    authScopes(_, {}, { user }) {
+      if (!user) throw new GraphQLError('User not found');
+      return Boolean(user.admin);
+    },
+    async resolve(query, _, {}, { user: me }) {
+      if (!me) throw new GraphQLError('User not found');
+      return prisma.logEntry.findMany({
+        ...query,
+        include: {
+          user: true,
+        },
+        orderBy: {
+          happenedAt: 'desc',
+        },
+      });
+    },
+  }),
+);
--- packages/api/src/modules/logs/types/log-entry.ts
+++ packages/api/src/modules/logs/types/log-entry.ts
@@ -1,1 +1,15 @@
-// from old.ts
+import { builder } from '#lib';
+import { DateTimeScalar } from '#modules/global';
+import {} from '../index.js';
+
+export const LogType = builder.prismaNode('LogEntry', {
+  id: { field: 'id' },
+  fields: (t) => ({
+    happenedAt: t.expose('happenedAt', { type: DateTimeScalar }),
+    user: t.relation('user', { nullable: true }),
+    area: t.exposeString('area'),
+    action: t.exposeString('action'),
+    target: t.exposeString('target', { nullable: true }),
+    message: t.exposeString('message'),
+  }),
+});
