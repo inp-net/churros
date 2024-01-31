84a3b27cb943be2e80d99e5d840f07af46216590
Ewen Le Bihan <hey@ewen.works>
2024-01-31 16:25:36 +0100

    WIP: reorganize schools module

--- packages/api/src/modules/services/resolvers/query.services.ts
+++ packages/api/src/modules/services/resolvers/query.services.ts
@@ -1,2 +1,27 @@
-// from old.ts
+import { builder, prisma } from '#lib';
+import {} from '#modules/global';
+import { ServiceType } from '../index.js';
 // TODO split into  group.services, student-association.services, school.services and query.all-services for admins
+
+builder.queryField('services', (t) =>
+  t.prismaField({
+    type: [ServiceType],
+    args: {
+      schoolUid: t.arg.string({ required: false }),
+      groupUid: t.arg.string({ required: false }),
+      studentAssociationUid: t.arg.string({ required: false }),
+    },
+    async resolve(query, _, { schoolUid, groupUid, studentAssociationUid }) {
+      const services = await prisma.service.findMany({
+        ...query,
+        where: {
+          school: { uid: schoolUid ?? undefined },
+          group: { uid: groupUid ?? undefined },
+          studentAssociation: { uid: studentAssociationUid },
+        },
+        orderBy: [{ importance: 'desc' }, { name: 'asc' }],
+      });
+      return services;
+    },
+  }),
+);
--- packages/api/src/modules/services/types/logo-source-type.ts
+++ packages/api/src/modules/services/types/logo-source-type.ts
@@ -1,1 +1,6 @@
-// from old.ts
+import { builder } from '#lib';
+import {} from '#modules/global';
+import { LogoSourceType } from '@prisma/client';
+import {} from '../index.js';
+
+export const LogoSourceTypeEnum = builder.enumType(LogoSourceType, { name: 'LogoSourceType' });
