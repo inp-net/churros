f1e7c06f17e0f3886e5ffb1055e511803d2cfb5c
Ewen Le Bihan <hey@ewen.works>
2024-01-31 16:30:51 +0100

    WIP: reorganize module student-associations

--- packages/api/src/modules/student-associations/resolvers/query.student-association.ts
+++ packages/api/src/modules/student-associations/resolvers/query.student-association.ts
@@ -1,1 +1,37 @@
-// from old.ts
+import { builder, prisma } from '#lib';
+import {} from '#modules/global';
+import { StudentAssociationType } from '../index.js';
+
+builder.queryField('studentAssociations', (t) =>
+  t.prismaField({
+    type: [StudentAssociationType],
+    authScopes(_, {}, { user }) {
+      return Boolean(user);
+    },
+    args: {
+      canContributeOnly: t.arg.boolean({ required: false }),
+    },
+    async resolve(query, _, { canContributeOnly }, { user }) {
+      return prisma.studentAssociation.findMany({
+        ...query,
+        where:
+          canContributeOnly && user?.major
+            ? {
+                contributionOptions: {
+                  some: {
+                    offeredIn: {
+                      majors: {
+                        some: {
+                          id: user.major.id,
+                        },
+                      },
+                    },
+                  },
+                },
+              }
+            : {},
+        orderBy: { updatedAt: 'desc' },
+      });
+    },
+  }),
+);
--- packages/api/src/modules/student-associations/resolvers/query.student-associations.ts
+++ packages/api/src/modules/student-associations/resolvers/query.student-associations.ts
@@ -1,2 +1,31 @@
-// from old.ts
+import { builder, prisma } from '#lib';
+import {} from '#modules/global';
+import { StudentAssociationType } from '../index.js';
 // TODO split into user.student-associations, school.student-associations, and query.all-student-associations for admins
+
+builder.queryField('studentAssociation', (t) =>
+  t.prismaField({
+    type: StudentAssociationType,
+    args: {
+      uid: t.arg.string(),
+    },
+    async resolve(query, _, { uid }) {
+      return prisma.studentAssociation.findUniqueOrThrow({
+        ...query,
+        where: { uid },
+        include: {
+          groups: {
+            where: {
+              // Type Club or Association
+              type: { in: ['Association', 'Club', 'StudentAssociationSection'] },
+            },
+            orderBy: {
+              name: 'asc',
+            },
+          },
+          links: true,
+        },
+      });
+    },
+  }),
+);
