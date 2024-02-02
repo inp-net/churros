20b9bf6a1785e717fb8d019b46915d5032c0f289
Ewen Le Bihan <hey@ewen.works>
2024-02-02 15:44:07 +0100

    feat(maintenance): add link to services status page

--- packages/api/src/server/maintenance.ts
+++ packages/api/src/server/maintenance.ts
@@ -4,7 +4,9 @@ export const maintenance = express();
 
 maintenance.get('/', (_, res) => {
   console.info(`Hit maintenance page`);
-  res.send(`<h1>Maintenance en cours</h1><p>On revient dès que possible!</p>`);
+  res.send(
+    `<h1>Maintenance en cours</h1><p>On revient dès que possible!</p><a href="https://status.inpt.fr/status/services">État des services</a>`,
+  );
 });
 
 maintenance.get('/*', (_, res) => {
