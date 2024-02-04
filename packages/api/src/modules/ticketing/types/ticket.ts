63c4ba2a8540c257eff33eed6ce091ade645ebc3
Ewen Le Bihan <hey@ewen.works>
2024-02-04 22:16:27 +0100

    fix: forward-port 1.44.1 hotfix correction (see 1d587118d9f0156739725cae25ca1f75f9585bfb)

--- packages/api/src/modules/ticketing/types/ticket.ts
+++ packages/api/src/modules/ticketing/types/ticket.ts
@@ -36,7 +36,7 @@ export const TicketType = builder.prismaNode('Ticket', {
     registrations: t.relation('registrations', {
       authScopes: { loggedIn: true },
       query(_, { user }) {
-        if (!user) return {};
+        if (!user) return { where: { none: {} } };
         if (user.admin) return {};
 
         return {
