1d587118d9f0156739725cae25ca1f75f9585bfb
Ewen Le Bihan <hey@ewen.works>
2024-02-04 22:10:07 +0100

    fix: woops la dinguerie

--- packages/api/src/objects/tickets.ts
+++ packages/api/src/objects/tickets.ts
@@ -84,7 +84,7 @@ export const TicketType = builder.prismaNode('Ticket', {
     registrations: t.relation('registrations', {
       authScopes: { loggedIn: true },
       query(_, { user }) {
-        if (!user) return {};
+        if (!user) return { where: { none: {} } };
         if (user.admin) return {};
 
         return {
