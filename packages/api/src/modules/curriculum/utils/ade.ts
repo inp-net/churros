9b672e1515e1c021ea7f5bd1f9075d504baee057
Ewen Le Bihan <hey@ewen.works>
2024-02-08 09:54:56 +0100

    fix(ade): pass token to feed scraper

--- packages/api/src/modules/curriculum/utils/ade.ts
+++ packages/api/src/modules/curriculum/utils/ade.ts
@@ -1,19 +1,24 @@
 import { log } from '#lib';
 
-export async function nextExamDates(user: {
-  schoolUid: string | null;
-}): Promise<Record<string, Date>> {
+export async function nextExamDates(
+  user: {
+    schoolUid: string | null;
+  },
+  token: string,
+): Promise<Record<string, Date>> {
   if (!user.schoolUid) return {};
-  const exams = (await fetch(`https://calendrier-n7.inpt.fr/${user.schoolUid}/next-exams`).then(
-    async (response) => {
-      if (response.status !== 200) {
-        await log('ADE', 'error', { error: await response.text() });
-        return {};
-      }
-
-      return response.json();
+  const exams = (await fetch(`https://calendrier-n7.inpt.fr/${user.schoolUid}/next-exams`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
-  )) as Record<string, { starts_at: string }>;
+  }).then(async (response) => {
+    if (response.status !== 200) {
+      await log('ADE', 'error', { error: await response.text() });
+      return {};
+    }
+
+    return response.json();
+  })) as Record<string, { starts_at: string }>;
 
   return Object.fromEntries(
     Object.entries(exams).map(([code, exam]) => [code, new Date(exam.starts_at)]),
