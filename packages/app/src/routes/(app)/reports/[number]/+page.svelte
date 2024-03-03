1e86e7114f9bc5a4250b127079f54b8cd3ae0305
Ewen Le Bihan <hey@ewen.works>
2024-03-03 20:48:52 +0100

    WIP: implement /reports/*, explain duplicated reports in /reports/[number]

--- packages/app/src/routes/(app)/reports/[number]/+page.svelte
+++ packages/app/src/routes/(app)/reports/[number]/+page.svelte
@@ -1,74 +1,100 @@
 <script lang="ts">
   import Badge from '$lib/components/Badge.svelte';
   import ButtonBack from '$lib/components/ButtonBack.svelte';
+  import IconHelpOutline from '~icons/mdi/help-circle-outline';
   import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
+  import LoadingSpinner from '$lib/components/LoadingSpinner.svelte';
   import CardComment from '$lib/components/CardComment.svelte';
   import { ISSUE_STATE_DISPLAY } from '$lib/display';
   import { IssueState } from '$lib/zeus';
   import IconOpenExternal from '~icons/mdi/open-in-new';
-  import type { PageData } from './$types';
+  import type { PageData } from './$houdini';
+  import { tooltip } from '$lib/tooltip';
 
   export let data: PageData;
-
-  const { title, bodyHtml, importance, url, difficulty, number, state, comments } = data.issue;
+  $: ({ ReportDetails } = data);
 </script>
 
 <div class="content">
-  <p class="overtitle">
-    <ButtonBack go="..">Tout les signalements</ButtonBack>
-  </p>
-  <h1><span class="muted number">#{number}</span> {title}</h1>
+  {#if !$ReportDetails.data?.issue}
+    <LoadingSpinner /> Chargement…
+  {:else}
+    {@const {
+      title,
+      bodyHtml,
+      importance,
+      url,
+      difficulty,
+      number,
+      state,
+      comments,
+      duplicatedFrom,
+    } = $ReportDetails.data.issue}
+    <p class="overtitle">
+      <ButtonBack go="..">Tout les signalements</ButtonBack>
+    </p>
+    <h1>
+      <span class="muted number">#{number}</span>
+      {#if duplicatedFrom}
+        <span
+          use:tooltip={`Ton signalement #${duplicatedFrom} est un duplicata de #${number}`}
+          class="muted help"><IconHelpOutline></IconHelpOutline></span
+        >
+      {/if}
+      {title}
+    </h1>
 
-  <section class="metadata">
-    {#if difficulty !== undefined && difficulty !== null}
+    <section class="metadata">
+      {#if difficulty !== undefined && difficulty !== null}
+        <div class="metadata-item">
+          <span class="typo-field-label">Difficulté</span>
+          <progress max="100" value={difficulty * 100} />
+        </div>
+      {/if}
+      {#if importance !== undefined && importance !== null}
+        <div class="metadata-item">
+          <span class="typo-field-label">Importance</span>
+          <progress max="100" value={importance * 100} />
+        </div>
+      {/if}
       <div class="metadata-item">
-        <span class="typo-field-label">Difficulté</span>
-        <progress max="100" value={difficulty * 100} />
+        <span class="typo-field-label">État</span>
+        <Badge theme={state === IssueState.Closed ? 'success' : 'warning'}>
+          {ISSUE_STATE_DISPLAY.get(state)}
+        </Badge>
       </div>
-    {/if}
-    {#if importance !== undefined && importance !== null}
-      <div class="metadata-item">
-        <span class="typo-field-label">Importance</span>
-        <progress max="100" value={importance * 100} />
-      </div>
-    {/if}
-    <div class="metadata-item">
-      <span class="typo-field-label">État</span>
-      <Badge theme={state === IssueState.Closed ? 'success' : 'warning'}>
-        {ISSUE_STATE_DISPLAY.get(state)}
-      </Badge>
-    </div>
-  </section>
+    </section>
 
-  <section class="body" data-user-html>
-    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-    {@html bodyHtml}
-  </section>
+    <section class="body" data-user-html>
+      <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+      {@html bodyHtml}
+    </section>
 
-  <section class="comments">
-    <h2>Commentaires</h2>
-    <ul>
-      {#each comments as comment}
-        <CardComment
-          readonly
-          id=""
-          bodyHtml={comment.bodyHtml}
-          body={comment.body}
-          author={{
-            uid: '',
-            externalHref: comment.authorGitlabUrl,
-            fullName: comment.authorName,
-            pictureFile: comment.authorAvatarUrl,
-          }}
-          createdAt={new Date(comment.addedAt)}
-        ></CardComment>
-      {/each}
-    </ul>
-  </section>
+    <section class="comments">
+      <h2>Commentaires</h2>
+      <ul>
+        {#each comments as comment}
+          <CardComment
+            readonly
+            id=""
+            bodyHtml={comment.bodyHtml}
+            body={comment.body}
+            author={{
+              uid: '',
+              externalHref: comment.authorGitlabUrl,
+              fullName: comment.authorName,
+              pictureFile: comment.authorAvatarUrl,
+            }}
+            createdAt={new Date(comment.addedAt)}
+          ></CardComment>
+        {/each}
+      </ul>
+    </section>
 
-  <section class="actions">
-    <ButtonSecondary href={url} icon={IconOpenExternal}>Plus d'infos sur Gitlab</ButtonSecondary>
-  </section>
+    <section class="actions">
+      <ButtonSecondary href={url} icon={IconOpenExternal}>Plus d'infos sur Gitlab</ButtonSecondary>
+    </section>
+  {/if}
 </div>
 
 <style>
