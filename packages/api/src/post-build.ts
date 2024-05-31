ca1ba28896ae8709184cc5b164e8f15817396fbe
Ewen Le Bihan <hey@ewen.works>
2024-05-31 17:25:39 +0200

    build(mails): include src/mail-templates/*.mjml in built files

--- packages/api/src/post-build.ts
+++ packages/api/src/post-build.ts
@@ -1,11 +1,29 @@
 const start = performance.now();
 
+import { readdir, readFile, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { isMJMLTemplateFilename, mailTemplatesDirectory } from './mail-templates/props.js';
 import { writeSchema } from './schema.js';
 await writeSchema();
 
 console.info(
   '✨ Schema exported in \u001B[36;1m%s ms\u001B[0m',
   Number((performance.now() - start).toPrecision(3)),
+);
+
+const templates = await readdir(mailTemplatesDirectory);
+
+const templatesDestinationDirectory = mailTemplatesDirectory.replace('src/', 'build/src/');
+
+await Promise.all(
+  templates.map(async (template) => {
+    if (!isMJMLTemplateFilename(template)) return;
+    await writeFile(
+      path.join(templatesDestinationDirectory, template),
+      await readFile(path.join(mailTemplatesDirectory, template), 'utf8'),
+    );
+    console.info('📄 Copied mail template \u001B[36;1m%s\u001B[0m', template);
+  }),
 );
 
 // eslint-disable-next-line unicorn/no-process-exit
