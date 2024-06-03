bdb2d4272cef941c8d0b256ef87aa45c582b059a
Ewen Le Bihan <hey@ewen.works>
2024-06-03 14:48:24 +0200

    feat(bot accounts): add mutation to create bot accounts, don't show bots in birthdays and search results

---
+++ packages/api/src/modules/users/resolvers/mutation.create-bot.ts
@@ -0,0 +1,47 @@
+import { builder, prisma } from '#lib';
+import { UserType } from '../types/user.js';
+
+builder.mutationField('createBot', (t) =>
+  t.prismaField({
+    type: UserType,
+    args: {
+      major: t.arg.string({ description: 'UID de la filière', required: false }),
+      uid: t.arg.string({ description: 'UID du compte bot' }),
+      name: t.arg.string({ description: 'Nom du compte bot' }),
+    },
+    async authScopes(_, { major }, { user }) {
+      if (!user) return false;
+      if (user.admin) return true;
+      if (!major) return false;
+      const majorStudentAssociations = await prisma.studentAssociation.findMany({
+        where: {
+          school: {
+            majors: {
+              some: {
+                uid: major,
+              },
+            },
+          },
+        },
+      });
+
+      return user.adminOfStudentAssociations.some((a) =>
+        majorStudentAssociations.some((m) => m.id === a.id),
+      );
+    },
+    async resolve(query, _, { major, uid, name }) {
+      return prisma.user.create({
+        ...query,
+        data: {
+          uid,
+          firstName: name,
+          lastName: '',
+          bot: true,
+          major: major ? { connect: { uid: major } } : undefined,
+          email: `${uid}@local`,
+          graduationYear: 0,
+        },
+      });
+    },
+  }),
+);
