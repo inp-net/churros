5d673b7d6f600424341c05815269d9b5f83ffd14
Ewen Le Bihan <hey@ewen.works>
2024-06-05 13:55:44 +0200

    build: fix build

--- packages/app/src/routes/(app)/register/+layout.ts
+++ packages/app/src/routes/(app)/register/+layout.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
-import type { LayoutLoad } from './[[quickSignupCode]]/$types';
+import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = async ({ parent }) => {
   const data = await parent();
