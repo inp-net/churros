b7d8a28f079196c88da640d29b15209799d23243
Ewen Le Bihan <hey@ewen.works>
2024-06-13 00:28:41 +0200

    fix(posts): don't request registrations of linked event when loggedout (closes #931)

--- packages/app/src/routes/(app)/posts/[group]/[uid]/+page.ts
+++ packages/app/src/routes/(app)/posts/[group]/[uid]/+page.ts
@@ -1,102 +1,107 @@
 import { loadQuery, Selector } from '$lib/zeus.js';
 import type { PageLoad } from './$types';
 
-export const _articleQuery = Selector('Article')({
-  id: true,
-  uid: true,
-  title: true,
-  bodyHtml: true,
-  visibility: true,
-  publishedAt: true,
-  notifiedAt: true,
-  canBeEdited: true,
-  group: {
-    id: true,
-    uid: true,
-    name: true,
-    pictureFile: true,
-    pictureFileDark: true,
-  },
-  pictureFile: true,
-  myReactions: true,
-  reactionCounts: true,
-  comments: [
-    {
-      first: 100,
-    },
-    {
-      edges: {
-        node: {
-          id: true,
-          author: { uid: true, fullName: true, pictureFile: true },
-          bodyHtml: true,
-          body: true,
-          inReplyToId: true,
-          createdAt: true,
-          updatedAt: true,
-        },
-      },
-    },
-  ],
-  event: {
+export const _articleQuery = (loggedIn: boolean) =>
+  Selector('Article')({
     id: true,
     uid: true,
     title: true,
-    startsAt: true,
-    endsAt: true,
-    frequency: true,
-    recurringUntil: true,
-    location: true,
-    placesLeft: true,
-    capacity: true,
-    descriptionHtml: true,
-    descriptionPreview: true,
+    bodyHtml: true,
+    visibility: true,
+    publishedAt: true,
+    notifiedAt: true,
+    canBeEdited: true,
+    group: {
+      id: true,
+      uid: true,
+      name: true,
+      pictureFile: true,
+      pictureFileDark: true,
+    },
     pictureFile: true,
+    myReactions: true,
+    reactionCounts: true,
+    comments: [
+      {
+        first: 100,
+      },
+      {
+        edges: {
+          node: {
+            id: true,
+            author: { uid: true, fullName: true, pictureFile: true },
+            bodyHtml: true,
+            body: true,
+            inReplyToId: true,
+            createdAt: true,
+            updatedAt: true,
+          },
+        },
+      },
+    ],
+    event: {
+      id: true,
+      uid: true,
+      title: true,
+      startsAt: true,
+      endsAt: true,
+      frequency: true,
+      recurringUntil: true,
+      location: true,
+      placesLeft: true,
+      capacity: true,
+      descriptionHtml: true,
+      descriptionPreview: true,
+      pictureFile: true,
+      links: {
+        name: true,
+        computedValue: true,
+      },
+      tickets: {
+        id: true,
+        uid: true,
+        ...(loggedIn
+          ? {
+              registrations: {
+                id: true,
+                beneficiary: true,
+                authorIsBeneficiary: true,
+                beneficiaryUser: { uid: true, firstName: true, lastName: true, fullName: true },
+                author: { uid: true },
+                paid: true,
+                ticket: {
+                  name: true,
+                },
+              },
+            }
+          : {}),
+        opensAt: true,
+        closesAt: true,
+        placesLeft: true,
+        capacity: true,
+        name: true,
+        descriptionHtml: true,
+        price: true,
+      },
+      group: {
+        uid: true,
+        pictureFile: true,
+        name: true,
+      },
+      contactMail: true,
+    },
     links: {
       name: true,
       computedValue: true,
     },
-    tickets: {
-      id: true,
-      uid: true,
-      registrations: {
-        id: true,
-        beneficiary: true,
-        authorIsBeneficiary: true,
-        beneficiaryUser: { uid: true, firstName: true, lastName: true, fullName: true },
-        author: { uid: true },
-        paid: true,
-        ticket: {
-          name: true,
-        },
-      },
-      opensAt: true,
-      closesAt: true,
-      placesLeft: true,
-      capacity: true,
-      name: true,
-      descriptionHtml: true,
-      price: true,
-    },
-    group: {
-      uid: true,
-      pictureFile: true,
-      name: true,
-    },
-    contactMail: true,
-  },
-  links: {
-    name: true,
-    computedValue: true,
-  },
-});
+  });
 
 export const load: PageLoad = async ({ fetch, params, parent }) => {
-  const { canEditGroup, token } = await parent();
+  const { canEditGroup, token, me } = await parent();
 
   const { article } = await loadQuery(
     {
-      article: [{ uid: params.uid, groupUid: params.group }, _articleQuery],
+      article: [{ uid: params.uid, groupUid: params.group }, _articleQuery(Boolean(me))],
     },
     { fetch, token },
   );
