2338000450e8a05056b7b5d9d2b993f53178ff3d
Ewen Le Bihan <hey@ewen.works>
2024-06-22 17:11:13 +0200

    fix(scripts): stub out buildinfo in dev
    c'est @valliet qui va être content :3

--- packages/app/src/lib/buildinfo.ts
+++ packages/app/src/lib/buildinfo.ts
@@ -1,2 +1,2 @@
-export const CURRENT_COMMIT = "0188dba1348d42e5658f0abe45f85fa945ca88fe" as string;
-export const CURRENT_VERSION = "1.63.1-rc.1" as string
+export const CURRENT_COMMIT = "dev" as string;
+export const CURRENT_VERSION = "dev" as string
--- scripts/generate-buildinfo.ts
+++ scripts/generate-buildinfo.ts
@@ -2,16 +2,20 @@ import { execa } from 'execa';
 import { writeFileSync } from 'node:fs';
 import path from 'node:path';
 
+const stub = process.argv.length >= 2 && process.argv[2] === '--stub';
+
 async function git(args: string): Promise<string> {
   const { stdout } = await execa('git', args.split(' '));
   return stdout;
 }
 
-const hash = await git('rev-parse HEAD').then((hash) => hash.trim());
+const hash = stub ? 'dev' : await git('rev-parse HEAD').then((hash) => hash.trim());
 const toplevel = await git('rev-parse --show-toplevel');
-const tag = await git(
-  'for-each-ref refs/tags --sort=-v:refname --format=%(refname:short) --count=1',
-).then((tag) => tag.trim().replace(/^v/, ''));
+const tag = stub
+  ? 'dev'
+  : await git('for-each-ref refs/tags --sort=-v:refname --format=%(refname:short) --count=1').then(
+      (tag) => tag.trim().replace(/^v/, ''),
+    );
 
 for (const relativePath of [
   'packages/app/src/lib/buildinfo.ts',
