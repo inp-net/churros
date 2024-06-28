aa383f4c462ed797704a26aad5163117c4852530
Ewen Le Bihan <hey@ewen.works>
2024-06-29 00:00:42 +0200

    build: fix prisma client import

--- packages/api/src/server/handover-pdf.ts
+++ packages/api/src/server/handover-pdf.ts
@@ -1,5 +1,5 @@
 import { prisma } from '#lib';
-import type { School } from '@prisma/client';
+import type { School } from '@centraverse/db/prisma';
 import pdfMakePrinter from 'pdfmake';
 import type { TFontDictionary } from 'pdfmake/interfaces';
 import { api } from './express.js';
