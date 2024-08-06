983990e82847e750a8f33e303f3c83673947e489
Ewen Le Bihan <hey@ewen.works>
2024-08-07 01:21:24 +0200

    feat(ticketing): implement (most?) mutations needed to tickets editing

--- packages/api/src/modules/links/types/has-links.ts
+++ packages/api/src/modules/links/types/has-links.ts
@@ -1,9 +1,10 @@
 import { builder, splitID } from '#lib';
 import { EventType } from '#modules/events';
 import { ArticleType } from '#modules/posts';
+import { TicketType } from '#modules/ticketing';
 
 export const HasLinks = builder.unionType('HasLinks', {
-  types: [EventType, ArticleType],
+  types: [EventType, ArticleType, TicketType],
   resolveType({ id }) {
     switch (splitID(id)[0]) {
       case 'Event': {
@@ -11,6 +12,9 @@ export const HasLinks = builder.unionType('HasLinks', {
       }
       case 'Article': {
         return ArticleType;
+      }
+      case 'Ticket': {
+        return TicketType;
       }
       default: {
         throw new Error(
