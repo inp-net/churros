9553dfd23a5706c0a1a2fdd48188a959d22893c2
Ewen Le Bihan <hey@ewen.works>
2024-08-23 09:07:35 +0200

    feat(ssr): enable ssr globally on dev

--- packages/app/src/routes/+layout.ts
+++ packages/app/src/routes/+layout.ts
@@ -1,10 +1,8 @@
-import { dev } from '$app/environment';
 import { load_RootLayout } from '$houdini';
 import { setDefaultOptions } from 'date-fns';
 import fr from 'date-fns/locale/fr/index.js';
 
-// FIXME: Makes vite hang on dev because of too many open files. Removing $lib/components/index.ts might fix this
-export const ssr = !dev;
+export const ssr = true;
 
 export const trailingSlash = 'always';
 
