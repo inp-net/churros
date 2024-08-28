fe18c212ed2de02bc336b5d314a1ea08caa50c67
Ewen Le Bihan <hey@ewen.works>
2024-08-28 20:43:11 +0200

    fix(profile): 404 on profiles with 2-chars uids

--- packages/app/src/params/uid.ts
+++ packages/app/src/params/uid.ts
@@ -2,4 +2,4 @@ import type { ParamMatcher } from '@sveltejs/kit';
 import { _RESERVED_USERNAMES } from '../routes/check-uid/[uid]/+server';
 
 export const match: ParamMatcher = (param) =>
-  /^[\w-]{3,255}$/.test(param) && !_RESERVED_USERNAMES.has(param);
+  /^[\w-]{2,255}$/.test(param) && !_RESERVED_USERNAMES.has(param);
