d3b0534224ab45c4c8305c4577aa3a0cd6c2de5c
Ewen Le Bihan <hey@ewen.works>
2024-09-02 23:58:01 +0200

    feat(uid): lowercase all UIDs on input

--- packages/api/src/modules/global/types/uid.ts
+++ packages/api/src/modules/global/types/uid.ts
@@ -1,8 +1,29 @@
 import { builder } from '#lib';
-import { RegularExpression } from 'graphql-scalars';
+import { GraphQLError, Kind } from 'graphql';
 
 // Scalar must allow 2-chars UIDs because we have some UIDs with 2 chars already.
 export const UID_REGEX_PATTERN = /^[\w-]{2,255}$/;
 
-const UIDScalarResolver = new RegularExpression('UID', UID_REGEX_PATTERN);
-export const UIDScalar = builder.addScalarType('UID', UIDScalarResolver, {});
+export const UIDScalar = builder.scalarType('UID', {
+  description: `Un nom d'utilisateur·ice unique permettant d'identifier un groupe, une personne, une AE, etc. Doit correspondre à l'expression régulière ${UID_REGEX_PATTERN}`,
+  serialize(value) {
+    if (value !== null && !UID_REGEX_PATTERN.test(value)) 
+      throw new GraphQLError(`Identifiant "${value}" invalide`);
+    
+    return value.toLowerCase();
+  },
+  parseValue(value) {
+    if (!value || !UID_REGEX_PATTERN.test(value.toString()))
+      throw new GraphQLError(`Identifiant "${value}" invalide`);
+    return value.toString().toLowerCase();
+  },
+  parseLiteral(ast) {
+    if (ast.kind !== Kind.STRING) {
+      throw new GraphQLError(`Can only validate strings as ${name} but got a: ${ast.kind}`, {
+        nodes: [ast],
+      });
+    }
+
+    return ast.value;
+  },
+});
