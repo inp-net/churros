9d35981cbcc5952c5c506a939bcce98e53cdda75
Ewen Le Bihan <hey@ewen.works>
2024-09-04 21:03:51 +0200

    fix(handover.pdf): use 302 redirect instead of 301

--- packages/app/src/routes/(app)/groups/[uid]/handover.pdf/+server.ts
+++ packages/app/src/routes/(app)/groups/[uid]/handover.pdf/+server.ts
@@ -2,5 +2,5 @@ import { env } from '$env/dynamic/public';
 import { redirect } from '@sveltejs/kit';
 
 export async function GET(event) {
-  redirect(301, new URL(`/print-handover/${event.params.uid}`, env.PUBLIC_API_URL));
+  redirect(302, new URL(`/print-handover/${event.params.uid}`, env.PUBLIC_API_URL));
 }
