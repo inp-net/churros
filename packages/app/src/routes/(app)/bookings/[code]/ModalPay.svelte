dc1989fc998879ed537eb56c1cb0fe518cd5bcee
Ewen Le Bihan <hey@ewen.works>
2024-09-09 22:08:06 +0200

    feat(pay modal): tell sentry when a payment goes wrong

--- packages/app/src/routes/(app)/bookings/[code]/ModalPay.svelte
+++ packages/app/src/routes/(app)/bookings/[code]/ModalPay.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
   import { page } from '$app/stores';
+  import * as Sentry from '@sentry/sveltekit';
   import {
     fragment,
     graphql,
@@ -106,6 +107,7 @@
     paymentError = mutationSucceeded('payBooking', result)
       ? ''
       : mutationErrorMessages('payBooking', result).join('\n\n');
+    if (paymentError) Sentry.captureMessage(`Booking payment failed with ${paymentError}`, 'error');
     paymentInProgress = false;
   }
 
