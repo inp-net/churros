e27bc8060d80035947b3fe930d127041cba91cb3
Ewen Le Bihan <hey@ewen.works>
2024-09-12 02:42:50 +0200

    feat(capacitor): make oauth login work‼️

--- packages/app/src/lib/oauth.ts
+++ packages/app/src/lib/oauth.ts
@@ -1,5 +1,6 @@
 import { env } from '$env/dynamic/public';
 import { AUTH_URL } from '$lib/env';
+import { Capacitor } from '@capacitor/core';
 import type { Cookies } from '@sveltejs/kit';
 
 export function oauthEnabled(): boolean {
@@ -14,7 +15,12 @@ export function oauthLoginBypassed(event: { url: URL }): boolean {
 
 export function oauthInitiateLoginURL(event: { url: URL }): URL {
   const url = new URL(AUTH_URL + '/oauth2');
+
   url.searchParams.set('from', event.url.searchParams.get('from') ?? '/');
+  // Causes the API to return a token in the redirect URL instead of setting a cookie,
+  // and also redirects to the churros.app URL scheme instead of the frontend web origin
+  if (Capacitor.isNativePlatform()) url.searchParams.set('native', '1');
+
   return url;
 }
 
--- packages/app/src/routes/(app)/login/+page.ts
+++ packages/app/src/routes/(app)/login/+page.ts
@@ -1,6 +1,35 @@
+import { goto } from '$app/navigation';
 import { oauthEnabled, oauthInitiateLoginURL, oauthLoginBypassed } from '$lib/oauth';
+import { Capacitor, CapacitorCookies } from '@capacitor/core';
+import { InAppBrowser } from '@capgo/inappbrowser';
 import { redirect } from '@sveltejs/kit';
+import { addYears } from 'date-fns';
 
 export async function load(event) {
-  if (oauthEnabled() && !oauthLoginBypassed(event)) redirect(307, oauthInitiateLoginURL(event));
+  if (oauthEnabled() && !oauthLoginBypassed(event)) {
+    if (Capacitor.isNativePlatform()) {
+      await InAppBrowser.addListener('urlChangeEvent', async (event) => {
+        const url = new URL(event.url);
+        if (url.protocol === 'app.churros:') {
+          await CapacitorCookies.setCookie({
+            key: 'authed_via',
+            value: 'oauth2',
+            expires: addYears(new Date(), 1).toISOString(),
+            path: '/',
+          });
+          await CapacitorCookies.setCookie({
+            key: 'token',
+            value: url.searchParams.get('token') ?? '',
+            expires: addYears(new Date(), 1).toISOString(),
+            path: '/',
+          });
+          await InAppBrowser.close();
+          await goto('/login/done');
+        }
+      });
+      await InAppBrowser.openWebView({ url: oauthInitiateLoginURL(event).toString() });
+    } else {
+      redirect(307, oauthInitiateLoginURL(event));
+    }
+  }
 }
