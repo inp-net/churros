c3b00f6e97d578be0cb5ac6cbb952a6d7347f4d0
Ewen Le Bihan <hey@ewen.works>
2024-09-12 14:36:05 +0200

    feat(native): make logout work

--- packages/app/src/routes/(app)/logout/+page.svelte
+++ packages/app/src/routes/(app)/logout/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
   import { goto } from '$app/navigation';
+  import { Capacitor } from '@capacitor/core';
+  import { InAppBrowser } from '@capgo/inappbrowser';
   import { onMount } from 'svelte';
   import type { PageData } from './$types';
 
   export let data: PageData;
 
   onMount(async () => {
-    if (/^https?:\/\//.test(data.next)) window.location.href = data.next;
-    else await goto(data.next);
+    if (/^https?:\/\//.test(data.next)) {window.location.href = data.next;}
+    else {
+      if (Capacitor.isNativePlatform() && data.next.startsWith('https://')) {
+        await InAppBrowser.openWebView({
+          url: data.next,
+        });
+      }
+      await goto(data.next);
+    }
   });
 </script>
