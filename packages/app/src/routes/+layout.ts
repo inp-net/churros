82f91d4f9742262ec624257d9a75df404e5a3ef0
Ewen Le Bihan <hey@ewen.works>
2024-09-12 12:57:17 +0200

    feat(android): make back gesture/button go back in history

--- packages/app/src/routes/+layout.ts
+++ packages/app/src/routes/+layout.ts
@@ -1,4 +1,5 @@
 import { load_RootLayout } from '$houdini';
+import { App } from '@capacitor/app';
 import { setDefaultOptions } from 'date-fns';
 import fr from 'date-fns/locale/fr/index.js';
 
@@ -10,6 +11,11 @@ export async function load(event) {
   setDefaultOptions({
     weekStartsOn: 1,
     locale: fr,
+  });
+  App.addListener('backButton', (event) => {
+    // TODO close open drawer when there's one, instead of going back in history
+    if (event.canGoBack) window.history.back();
+    else App.exitApp();
   });
   return await load_RootLayout({ event });
 }
