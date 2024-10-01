a2ca8fb0a237acc1c3bb57cd1b98f311bae7fa5e
Ewen Le Bihan <hey@ewen.works>
2024-10-01 13:53:32 +0200

    WIP

--- packages/app/src/routes/(app)/settings/+layout.svelte
+++ packages/app/src/routes/(app)/settings/+layout.svelte
@@ -24,6 +24,7 @@
   import IconProfile from '~icons/msl/person-outline';
   import IconSpecialOffer from '~icons/msl/percent';
   import type { LayoutData } from './$houdini';
+  import { theme } from '$lib/theme';
 
   const UpdateLydiaPhone = graphql(`
     mutation UpdateLydiaPhone($lydiaPhone: String!) {
@@ -45,7 +46,7 @@
   // HINT: Don't forget to add an entry in packages/app/src/lib/navigation.ts for the top navbar's title and/or action buttons
 </script>
 
-<MaybeError result={$LayoutSettings} let:data={{ me }}>
+<MaybeError result={$LayoutSettings} let:data={{ me, themes }}>
   <Split mobilePart={$page.route.id === '/(app)/settings' ? 'left' : 'right'}>
     <div class="contents" slot="left">
       <Submenu>
@@ -54,7 +55,7 @@
         </SubmenuItem>
         <SubmenuItem
           icon={IconTheme}
-          subtext="Réglage par appareil"
+          subtext={themes.find((t) => t.localID === $theme.id)?.name ?? 'Par défaut'}
           href={route('/settings/theme')}
         >
           Thème
