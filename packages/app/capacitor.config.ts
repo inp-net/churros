2a86f6ad478b34f80db9716513e2cf7fb3141a43
Ewen Le Bihan <hey@ewen.works>
2024-10-14 01:01:30 +0200

    feat(native): notifications fkn work!!! (tested with a real APK)

--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -7,6 +7,11 @@ const config: CapacitorConfig = {
   android: {
     allowMixedContent: true,
   },
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+  },
 };
 
 export default config;
