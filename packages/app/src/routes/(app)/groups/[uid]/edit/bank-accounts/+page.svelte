c837c6fe11347717cbdd3a384f3748fb9f0329da
Ewen Le Bihan <hey@ewen.works>
2024-10-14 23:18:48 +0200

    fix(lydia): bank accounts could not be registered

--- packages/app/src/routes/(app)/groups/[uid]/edit/bank-accounts/+page.svelte
+++ packages/app/src/routes/(app)/groups/[uid]/edit/bank-accounts/+page.svelte
@@ -1,24 +1,27 @@
 <script lang="ts">
-  import { zeus } from '$lib/zeus';
-  import type { PageData } from './$types';
-
+  import { page } from '$app/stores';
   import { env } from '$env/dynamic/public';
-  import InputText from '$lib/components/InputText.svelte';
+  import { graphql } from '$houdini';
   import ButtonPrimary from '$lib/components/ButtonPrimary.svelte';
   import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
+  import InputText from '$lib/components/InputText.svelte';
+  import MaybeError from '$lib/components/MaybeError.svelte';
+  import { allLoaded, loaded, type MaybeLoading } from '$lib/loading';
+  import type { PageData } from './$houdini';
 
   export let data: PageData;
-
-  $: ({ group } = data);
-  $: lydiaAccountsOfGroup = group?.lydiaAccounts ?? [];
+  $: ({ PageGroupEditBankAccounts } = data);
 
   let loading = false;
   let phone = '';
   let password = '';
   let lydiaAccounts: Array<{ name: string; api_token_id: string; api_token: string }> = [];
 
-  const searchLydiaAccounts = async () => {
+  const searchLydiaAccounts = async (
+    alreadyLinkedAccounts: Array<{ name: MaybeLoading<string> }>,
+  ) => {
     if (loading) return;
+    if (!allLoaded(alreadyLinkedAccounts)) return;
     try {
       loading = true;
       const response = await fetch(`${env.PUBLIC_LYDIA_API_URL}/api/auth/login.json`, {
@@ -34,7 +37,7 @@
       business_list = business_list.filter(
         (business: { name: string; api_token_id: string; api_token: string }) =>
           // If the lydia account is already linked to the group, don't show it
-          !lydiaAccountsOfGroup.some((lydiaAccount) => lydiaAccount.name === business.name),
+          !alreadyLinkedAccounts.some((lydiaAccount) => lydiaAccount.name === business.name),
       );
       lydiaAccounts = business_list;
     } finally {
@@ -42,91 +45,106 @@
     }
   };
 
-  const addLydiaAccount = async (name: string, api_token_id: string, api_token: string) => {
+  const addLydiaAccount = async (
+    name: string,
+    api_token_id: string,
+    api_token: string,
+    groupUid: MaybeLoading<string>,
+  ) => {
     if (loading) return;
-    if (!group) return;
+    if (!loaded(groupUid)) return;
     try {
       loading = true;
-      const { upsertLydiaAccount } = await $zeus.mutate({
-        upsertLydiaAccount: [
-          {
-            name,
-            privateToken: api_token_id,
-            vendorToken: api_token,
-            groupUid: group.uid,
-          },
-          {
-            id: true,
-            name: true,
-            __typename: true,
-          },
-        ],
+
+      const RegisterLydiaAccount = graphql(`
+        mutation RegisterLydiaAccount(
+          $name: String!
+          $api_token_id: String!
+          $api_token: String!
+          $group: String!
+        ) {
+          upsertLydiaAccount(
+            name: $name
+            privateToken: $api_token_id
+            vendorToken: $api_token
+            groupUid: $group
+          ) {
+            id
+            name
+          }
+        }
+      `);
+
+      await RegisterLydiaAccount.mutate({
+        name,
+        api_token_id,
+        api_token,
+        group: groupUid,
       });
 
-      if (upsertLydiaAccount.__typename === 'LydiaAccount')
-        lydiaAccounts = lydiaAccounts.filter((lydiaAccount) => lydiaAccount.name !== name);
-      lydiaAccountsOfGroup = [
-        ...lydiaAccountsOfGroup,
-        {
-          id: upsertLydiaAccount.id,
-          name: upsertLydiaAccount.name,
-        },
-      ];
+      await PageGroupEditBankAccounts.fetch({
+        variables: { uid: $page.params.uid },
+      });
     } finally {
       loading = false;
     }
   };
 </script>
 
-<h2>Mes comptes</h2>
-<ul class="nobullet accounts">
-  {#each lydiaAccountsOfGroup as { name } (name)}
-    <li>
-      <p>{name}</p>
-      <p />
-      <div class="actions">
-        <ButtonSecondary danger>Supprimer</ButtonSecondary>
-      </div>
-    </li>
-  {/each}
-</ul>
+<MaybeError result={$PageGroupEditBankAccounts} let:data={{ group }}>
+  <h2>Mes comptes</h2>
+  <ul class="nobullet accounts">
+    {#each group.lydiaAccounts as { name } (name)}
+      <li>
+        <p>{name}</p>
+        <p />
+        <div class="actions">
+          <ButtonSecondary danger>Supprimer</ButtonSecondary>
+        </div>
+      </li>
+    {/each}
+  </ul>
 
-<hr />
+  <hr />
 
-<form class="add-account" on:submit|preventDefault={searchLydiaAccounts}>
-  <legend class="typo-title">Ajouter un compte Lydia</legend>
-  <p>
-    Pour ajouter un compte Lydia pro, vous devez d'abord créer un compte Lydia pour votre club. Pour
-    cela, vous pouvez utiliser l'application Lydia Pro sur votre téléphone. Une fois le compte créé,
-    vous pouvez l'ajouter ici en vous connectant avec vos identifiants Lydia. (Aucun mot de passe
-    n'est enregistré sur nos serveurs.)
-  </p>
+  <form
+    class="add-account"
+    on:submit|preventDefault={async () => searchLydiaAccounts(group.lydiaAccounts)}
+  >
+    <legend class="typo-title">Ajouter un compte Lydia</legend>
+    <p>
+      Pour ajouter un compte Lydia pro, vous devez d'abord créer un compte Lydia pour votre club.
+      Pour cela, vous pouvez utiliser l'application Lydia Pro sur votre téléphone. Une fois le
+      compte créé, vous pouvez l'ajouter ici en vous connectant avec vos identifiants Lydia. (Aucun
+      mot de passe n'est enregistré sur nos serveurs.)
+    </p>
 
-  <InputText type="tel" label="Numéro de téléphone" required bind:value={phone} />
-  <InputText type="password" label="Mot de passe Lydia" required bind:value={password} />
-  <section class="submits">
-    <ButtonPrimary {loading} submits>Rechercher</ButtonPrimary>
-  </section>
-</form>
-
-{#if lydiaAccounts.length > 0}
-  <form class="accounts-found">
-    <h2>Comptes disponibles</h2>
-    <ul class="accounts">
-      {#each lydiaAccounts as { name, api_token_id, api_token } (api_token_id)}
-        <li>
-          <InputText type="text" label="Nom" value={name} readonly />
-          <InputText type="text" label="Token" value={api_token} readonly />
-          <ButtonPrimary
-            {loading}
-            on:click={async () => addLydiaAccount(name, api_token_id, api_token)}
-            >Ajouter</ButtonPrimary
-          >
-        </li>
-      {/each}
-    </ul>
+    <InputText type="tel" label="Numéro de téléphone" required bind:value={phone} />
+    <InputText type="password" label="Mot de passe Lydia" required bind:value={password} />
+    <section class="submits">
+      <ButtonPrimary {loading} submits>Rechercher</ButtonPrimary>
+    </section>
   </form>
-{/if}
+
+  {#if lydiaAccounts.length > 0}
+    <form class="accounts-found">
+      <h2>Comptes disponibles</h2>
+      <ul class="accounts">
+        {#each lydiaAccounts as { name, api_token_id, api_token } (api_token_id)}
+          <li>
+            <InputText type="text" label="Nom" value={name} readonly />
+            <InputText type="text" label="Token" value={api_token} readonly />
+            <ButtonPrimary
+              {loading}
+              on:click={async () => addLydiaAccount(name, api_token_id, api_token, group.uid)}
+              >Ajouter</ButtonPrimary
+            >
+          </li>
+        {/each}
+      </ul>
+    </form>
+  {/if}
+</MaybeError>
 
 <style>
   form.add-account {
