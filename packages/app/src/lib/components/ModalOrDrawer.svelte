0612b0da614391900850e7de00130a2dc62da6d4
Ewen Le Bihan <hey@ewen.works>
2024-10-22 23:23:27 +0200

    feat(modals): improve open/close animations, add title prop and narrow prop

--- packages/app/src/lib/components/ModalOrDrawer.svelte
+++ packages/app/src/lib/components/ModalOrDrawer.svelte
@@ -1,13 +1,21 @@
 <script lang="ts">
+  import ButtonGhost from '$lib/components/ButtonGhost.svelte';
   import ModalDialog from '$lib/components/ModalDialog.svelte';
   import ModalDrawer from '$lib/components/ModalDrawer.svelte';
   import { isMobile } from '$lib/mobile';
   import { createEventDispatcher } from 'svelte';
+  import IconClose from '~icons/msl/close';
 
   const dispatch = createEventDispatcher<{ close: undefined }>();
 
   /** Signals that the content is quite tall */
   export let tall = false;
+
+  /** Signals that the content is quite narrow */
+  export let narrow = false;
+
+  /** Set a title. Uses the header slot. Also adds a close button to the right */
+  export let title = '';
 
   /** Don't render a trigger area */
   export let notrigger = false;
@@ -46,18 +54,37 @@
     bind:open={drawerOpen}
   >
     <slot {close}></slot>
-    <slot name="header" slot="header" {close}></slot>
+    <svelte:fragment slot="header">
+      {#if $$slots.header}
+        <slot {close} name="header"></slot>
+      {:else if title}
+        <header class="using-title">
+          <h2>{title}</h2>
+          <ButtonGhost help="Fermer" on:click={close}>
+            <IconClose />
+          </ButtonGhost>
+        </header>
+      {/if}
+    </svelte:fragment>
   </ModalDrawer>
 {:else}
   <ModalDialog
     on:close-by-outside-click={() => dispatch('close')}
     {tall}
+    {narrow}
     bind:element={dialogElement}
     bind:open={openDialog}
   >
-    {#if $$slots.header}
-      <header>
-        <slot {close} name="header"></slot>
+    {#if $$slots.header || title}
+      <header class:using-title={Boolean(title)}>
+        {#if $$slots.header}
+          <slot {close} name="header"></slot>
+        {:else}
+          <h2>{title}</h2>
+          <ButtonGhost help="Fermer" on:click={close}>
+            <IconClose />
+          </ButtonGhost>
+        {/if}
       </header>
     {/if}
     <slot {close}></slot>
@@ -70,5 +97,9 @@
     gap: 1rem;
     align-items: center;
     justify-content: space-between;
+  }
+
+  header.using-title :global(.button-ghost) {
+    font-size: 1.2em;
   }
 </style>
