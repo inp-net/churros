4beff51cea91509d7158a1ed21a5f1f4f75c778d
Gwen Le Bihan <hey@ewen.works>
2024-11-23 23:31:08 +0100

    feat(user deletion): lift webhook errors to users, log things

--- packages/api/src/modules/users/resolvers/mutation.delete-user.ts
+++ packages/api/src/modules/users/resolvers/mutation.delete-user.ts
@@ -1,6 +1,7 @@
-import { builder, ENV, prisma, purgeSessionsUser, UnauthorizedError } from '#lib';
+import { builder, ENV, log, prisma, purgeSessionsUser, UnauthorizedError } from '#lib';
 import { UserType } from '#modules/users/types';
 import { deleteLdapUser } from '@inp-net/ldap7/user';
+import { GraphQLError } from 'graphql';
 
 builder.mutationField('deleteUser', (t) =>
   t.prismaField({
@@ -9,6 +10,7 @@ builder.mutationField('deleteUser', (t) =>
     errors: {},
     async resolve(query, _, __, { user }) {
       if (!user) throw new UnauthorizedError();
+      await log('users', 'delete', { user }, user.id, user);
       return await prisma.$transaction(async (tx) => {
         const result = await tx.user.delete({
           ...query,
@@ -17,9 +19,29 @@ builder.mutationField('deleteUser', (t) =>
         await purgeSessionsUser(user.uid);
         if (process.env['NODE_ENV'] !== 'development') await deleteLdapUser(user.uid);
         if (ENV.USER_DELETED_WEBHOOK) {
-          const url = new URL(ENV.USER_DELETED_WEBHOOK);
-          url.searchParams.append('user', user.uid);
-          await fetch(url.toString());
+          try {
+            const url = new URL(ENV.USER_DELETED_WEBHOOK);
+            url.searchParams.append('user', user.uid);
+            await log(
+              'users',
+              'delete/webhook',
+              { user: user.uid, url: url.toString() },
+              user.id,
+              user,
+            );
+            await fetch(url.toString());
+          } catch (error) {
+            await log(
+              'users',
+              'delete/webhook/failed',
+              { error: error?.toString() },
+              user.id,
+              user,
+            );
+            throw new GraphQLError(
+              "Impossible de notifier la suppression aux autres services. Ton compte n'a pas été supprimé.",
+            );
+          }
         }
         return result;
       });
