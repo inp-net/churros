76ab8540ed9c8ae67baeb236b1e9ea6bf65689f7
Gwen Le Bihan <hey@ewen.works>
2024-11-24 19:03:05 +0100

    feat(announcements): improve announcements layouting on desktop

--- packages/app/src/routes/(app)/+layout.svelte
+++ packages/app/src/routes/(app)/+layout.svelte
@@ -2,17 +2,19 @@
   import { browser } from '$app/environment';
   import { goto, onNavigate } from '$app/navigation';
   import { page } from '$app/stores';
+  import { env } from '$env/dynamic/public';
   import { graphql } from '$houdini';
   import { CURRENT_VERSION } from '$lib/buildinfo';
-  import ButtonGhost from '$lib/components/ButtonGhost.svelte';
   import ButtonSecondary from '$lib/components/ButtonSecondary.svelte';
   import ModalChangelog from '$lib/components/ModalChangelog.svelte';
+  import ModalCreateGroup from '$lib/components/ModalCreateGroup.svelte';
   import NavigationBottom from '$lib/components/NavigationBottom.svelte';
   import NavigationSide from '$lib/components/NavigationSide.svelte';
   import NavigationTop, { type NavigationContext } from '$lib/components/NavigationTop.svelte';
   import OverlayQuickBookings from '$lib/components/OverlayQuickBookings.svelte';
   import PickGroup from '$lib/components/PickGroup.svelte';
   import QuickAccessList from '$lib/components/QuickAccessList.svelte';
+  import { allLoaded } from '$lib/loading';
   import { isMobile } from '$lib/mobile';
   import { mutate } from '$lib/mutations';
   import { addReferrer, refroute } from '$lib/navigation';
@@ -22,14 +24,10 @@
   import { toasts } from '$lib/toasts';
   import { setupViewTransition } from '$lib/view-transitions';
   import { setContext } from 'svelte';
-  import { syncToLocalStorage } from 'svelte-store2storage';
   import { writable } from 'svelte/store';
-  import IconClose from '~icons/mdi/close';
   import '../../design/app.scss';
   import type { PageData } from './$houdini';
-  import ModalCreateGroup from '$lib/components/ModalCreateGroup.svelte';
-  import { allLoaded } from '$lib/loading';
-  import { env } from '$env/dynamic/public';
+  import Announcements from './Announcements.svelte';
 
   onNavigate(setupViewTransition);
 
@@ -47,26 +45,7 @@
 
   const now = new Date();
 
-  function announcementHiddenByUser(id: string, hiddenAnnouncements: string[]): boolean {
-    return !browser || hiddenAnnouncements.includes(id);
-  }
-  const hiddenAnnouncements = writable<string[]>([]);
-  if (browser) syncToLocalStorage(hiddenAnnouncements, 'hidden_announcements');
-
-  const announcements = graphql(`
-    subscription AnnoncementUpdates {
-      announcementsNow {
-        id
-        title
-        bodyHtml
-        warning
-      }
-    }
-  `);
-  $: announcements.listen();
-
   $: scanningTickets = $page.url.pathname.endsWith('/scan/');
-  $: showingTicket = /\/bookings\/\w+\/$/.exec($page.url.pathname);
 
   // Select which student association to create groups linked to.
   // We get all the student associations the logged-in user can create groups on,
@@ -203,26 +182,7 @@
     </header>
 
     <div id="scrollable-area" class="contents-and-announcements">
-      <section class="announcements fullsize">
-        {#if !scanningTickets && !showingTicket && $announcements.data?.announcementsNow}
-          {#each $announcements.data?.announcementsNow.filter(({ id }) => !announcementHiddenByUser(id, $hiddenAnnouncements)) as { title, bodyHtml, warning, id } (id)}
-            <article class="announcement {warning ? 'warning' : 'primary'}">
-              <div class="text">
-                <strong>{title}</strong>
-                <div class="body" data-user-html>
-                  <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-                  {@html bodyHtml}
-                </div>
-              </div>
-              <ButtonGhost
-                on:click={() => {
-                  $hiddenAnnouncements = [...$hiddenAnnouncements, id];
-                }}><IconClose /></ButtonGhost
-              >
-            </article>
-          {/each}
-        {/if}
-      </section>
+      <Announcements />
       <div class="page-content">
         <slot />
       </div>
@@ -482,54 +442,6 @@
       border-right: solid 1px var(--scrollable-area-border-color);
       border-left: solid 1px var(--scrollable-area-border-color);
     }
-  }
-
-  .announcements {
-    display: flex;
-    flex-flow: column wrap;
-    width: 100%;
-    view-timeline-name: announcements;
-  }
-
-  .announcement {
-    display: flex;
-    column-gap: 1rem;
-    align-items: center;
-    justify-content: space-between;
-    padding: 0.5rem 2rem;
-    color: var(--text);
-    background: var(--bg);
-  }
-
-  .announcement .body :global(p) {
-    margin: 0;
-  }
-
-  @media (min-width: 900px) {
-    .announcements {
-      padding: 0 1rem;
-    }
-
-    .announcement:first-child {
-      border-top-left-radius: var(--radius-block);
-      border-top-right-radius: var(--radius-block);
-    }
-
-    .announcement:last-child {
-      border-bottom-right-radius: var(--radius-block);
-      border-bottom-left-radius: var(--radius-block);
-    }
-  }
-
-  .announcement:last-child {
-    margin-bottom: 2rem;
-  }
-
-  .announcement .text {
-    display: flex;
-    flex-wrap: wrap;
-    column-gap: 1rem;
-    align-items: center;
   }
 
   @keyframes spinner {
