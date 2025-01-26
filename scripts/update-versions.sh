e2c5d5a7764251b39906ecf11298865f49b0fa6b
Gwen Le Bihan <hey@ewen.works>
2025-01-26 12:55:34 +0000

    chore(changesets): try pushing after renovate changeset generation

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -2,6 +2,7 @@
 
 yarn install --mode skip-build
 node scripts/generate-renovate-changesets.js
+git push
 changeset version
 node scripts/release-user-facing-changelog.js
 yarn install --mode update-lockfile
