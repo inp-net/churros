a4998859ded6af9fcfccba6cc07d17abbe91b383
Gwen Le Bihan <hey@ewen.works>
2025-02-03 19:36:10 +0100

    fix(api): take PUBLIC_API_ORIGIN_WEB into account when computing Pictured.pictureURL

--- packages/api/src/modules/global/types/pictured.ts
+++ packages/api/src/modules/global/types/pictured.ts
@@ -60,7 +60,11 @@ export const PicturedInterface = builder.interfaceRef<Pictured>('Pictured').impl
         const filepath = dark ? pictureFileDark || pictureFile : pictureFile;
         if (!filepath) return '';
 
-        const result = new URL(ENV.PUBLIC_STORAGE_URL);
+        let result = new URL(ENV.PUBLIC_STORAGE_URL);
+
+        if (ENV.PUBLIC_API_ORIGIN_WEB)
+          result = new URL(result.toString().replace(result.origin, ENV.PUBLIC_API_ORIGIN_WEB));
+
         result.pathname = path.join(result.pathname, filepath);
         if (timestamp)
           result.searchParams.set('t', (updatedAt?.getTime() ?? Date.now()).toString());
