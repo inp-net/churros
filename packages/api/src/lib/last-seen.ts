4c7cfde683b6a634282df6ace4f2f62749f02200
Gwen Le Bihan <hey@ewen.works>
2025-02-03 19:38:08 +0100

    fix(api): don't try to parse already-object date in user last seen update

--- packages/api/src/lib/last-seen.ts
+++ packages/api/src/lib/last-seen.ts
@@ -1,5 +1,5 @@
 import { prisma, purgeSessionsUser } from '#lib';
-import { differenceInDays, parseISO } from 'date-fns';
+import { differenceInDays } from 'date-fns';
 
 /**
  * Express middleware that updates the last seen date of the logged-in user, in the background (it doesn't wait for the operation to complete)
@@ -19,9 +19,11 @@ export function updateUserLastSeen(
       // Have no lastSeenDate, or
       !user.user.lastSeenAt ||
       // the date is older than 1 day
-      Math.abs(differenceInDays(new Date(), parseISO(user.user.lastSeenAt))) > 1;
+      Math.abs(differenceInDays(new Date(), user.user.lastSeenAt)) > 1;
   } catch (error) {
-    console.error(`Error updating last seen date for user ${user.user.uid}: ${error}`);
+    console.error(
+      `Error updating last seen date ${user.user.lastSeenAt} for user ${user.user.uid}: ${error}`,
+    );
   }
 
   if (needsUpdate) {
