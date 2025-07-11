7bfcf53c928a9c913ff5763d17414479dedbb12f
Téo <pisentt@bde.enseeiht.fr>
2025-07-11 15:00:15 +0200

    fix(app): [uid=uid] page does not always load correctly due to race condition when the graphql query finish

--- packages/app/src/lib/buildinfo.ts
+++ packages/app/src/lib/buildinfo.ts
@@ -1,2 +1,2 @@
-export const CURRENT_COMMIT = '72ad22314b5c607b60a9011f8884de8a929c28fd' as string;
+export const CURRENT_COMMIT = '638cc1ca49d2cef41643566fb62f3bf1d7ad4b87' as string;
 export const CURRENT_VERSION = 'dev' as string;
